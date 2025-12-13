1f72df32ec6a3ae2fce04e355b809ab1387fa710
cy <chenyang08056032@163.com>
2025-12-13 14:22:46 +0800

    add  Qwen3_480B testcase

---
+++ test/srt/ascend_k8s/test_ascend_qwen3_480b_in3500_out1500_bs80.py
@@ -0,0 +1,82 @@
+import unittest
+
+from test_ascend_single_mix_utils import TestSingleMixUtils
+from test_ascend_single_mix_utils import NIC_NAME
+
+Qwen3_480B_MODEL_PATH = "/root/.cache/modelscope/hub/models/Qwen3-Coder-480B-A35B-Instruct-w8a8-QuaRot"
+Qwen3_480B_OTHER_ARGS = [
+    "--trust-remote-code",
+    "--nnodes",
+    "1",
+    "--node-rank",
+    "0",
+    "--attention-backend",
+    "ascend",
+    "--device",
+    "npu",
+    "--quantization",
+    "modelslim",
+    "--max-running-requests",
+    "96",
+    "--context-length",
+    "8192",
+    "--dtype",
+    "bfloat16"
+    "--chunked-prefill-size",
+    "32768",
+    "--max-prefill-tokens",
+    "458880",
+    "--disable-radix-cache",
+    "--moe-a2a-backend",
+    "deepep",
+    "--deepep-mode",
+    "auto",
+    "--tp-size",
+    "16",
+    "--dp-size",
+    "8",
+    "--enable-dp-attention",
+    "--enable-dp-lm-head",
+    "--mem-fraction-static",
+    "0.76",
+    "--cuda-graph-bs",
+    6,
+    8,
+    10,
+    12,
+]
+
+Qwen3_480B_ENVS = {
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT": "600",
+    "HCCL_BUFFSIZE": "2100",
+    "HCCL_SOCKET_IFNAME": NIC_NAME,
+    "GLOO_SOCKET_IFNAME": NIC_NAME,
+    "HCCL_OP_EXPANSION_MODE": "AIV",
+    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+    "SGLANG_ENABLE_SPEC_V2": "1",
+    "DISABLE_EAGLE3_QUANT": "1",
+}
+
+
+class TestQwen3_480B(TestSingleMixUtils):
+    model = Qwen3_480B_MODEL_PATH
+    other_args = Qwen3_480B_OTHER_ARGS
+    envs = Qwen3_480B_ENVS
+    dataset_name = "random"
+    request_rate = 5.5
+    max_concurrency = 80
+    input_len = 3500
+    output_len = 1500
+    random_range_ratio = 1
+    ttft = 5000
+    tpot = 50
+    output_token_throughput = 300
+
+    def test_qwen3_480b(self):
+        self.run_throughput()
+
+
+if __name__ == "__main__":
+    unittest.main()
