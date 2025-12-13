30063e6f49854cc57bb5de006ef1d3773433f736
cy <chenyang08056032@163.com>
2025-12-13 15:40:31 +0800

    fix

--- test/srt/ascend_k8s/test_ascend_qwen3_480b_in3500_out1500_bs80.py
+++ test/srt/ascend_k8s/test_ascend_qwen3_480b_in3500_out1500_bs80.py
@@ -67,6 +67,7 @@ class TestQwen3_480B(TestSingleMixUtils):
     dataset_name = "random"
     request_rate = 5.5
     max_concurrency = 80
+    num_prompts = int(max_concurrency) * 4
     input_len = 3500
     output_len = 1500
     random_range_ratio = 1
