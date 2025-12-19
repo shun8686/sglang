fcb04fe7a66e50686e98400c78c0e08bf3757ba3
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-19 16:06:14 +0800

    Update test_ascend_qwen3_235b_in2048_out2048_100ms.py

--- test/srt/ascend_k8s/test_ascend_qwen3_235b_in2048_out2048_100ms.py
+++ test/srt/ascend_k8s/test_ascend_qwen3_235b_in2048_out2048_100ms.py
@@ -8,25 +8,25 @@ QWEN3_235B_MODEL_PATH = "/root/.cache/modelscope/hub/models/vllm-ascend/Qwen3-23
 QWEN3_235B_A22B_EAGLE_MODEL_PATH = "/root/.cache/modelscope/hub/models/Qwen/Qwen3-235B-A22B-Eagle3"
 
 QWEN3_235B_ENVS = {
-    # "SGLANG_SET_CPU_AFFINITY": "1",
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "PATH": "/usr/local/Ascend/8.5.0/compiler/bisheng/bin:$PATH",
+    "ASCEND_HOME_PATH": "/usr/local/Ascend/ascend-toolkit/latest",
     "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
     "SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT": "600",
-    "HCCL_BUFFSIZE": "1200",
+    "HCCL_BUFFSIZE": "1600",
     "HCCL_SOCKET_IFNAME": NIC_NAME,
     "GLOO_SOCKET_IFNAME": NIC_NAME,
     "HCCL_OP_EXPANSION_MODE": "AIV",
     "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
     "SGLANG_ENABLE_SPEC_V2": "1",
-    "SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE": "1",
+    "DISABLE_EAGLE_QUANT": "1",
 }
 
 QWEN3_235B_OTHER_ARGS = (
     [
         "--trust-remote-code",
         "--nnodes",
-        "1",
-        "--node-rank",
-        "0",
+        "2",
         "--attention-backend",
         "ascend",
         "--device",
@@ -34,7 +34,7 @@ QWEN3_235B_OTHER_ARGS = (
         "--quantization",
         "modelslim",
         "--max-running-requests",
-        "756",
+        "1152",
         "--context-length",
         "8192",
         "--dtype",
@@ -48,29 +48,33 @@ QWEN3_235B_OTHER_ARGS = (
         "--speculative-draft-model-path",
         QWEN3_235B_A22B_EAGLE_MODEL_PATH,
         "--speculative-num-steps",
-        "3",
+        "1",
         "--speculative-eagle-topk",
         "1",
         "--speculative-num-draft-tokens",
-        "4",
+        "2",
         "--disable-radix-cache",
         "--moe-a2a-backend",
         "deepep",
         "--deepep-mode",
         "auto",
-        "--speculative-draft-model-quantization",
-        "unquant",
         "--tp",
-        "16",
+        "32",
         "--dp-size",
-        "4",
+        "32",
         "--enable-dp-attention",
         "--enable-dp-lm-head",
         "--mem-fraction-static",
-        "0.81",
+        "0.8",
         "--cuda-graph-bs",
-        "144",
-        "189",
+        "6",
+        "8",
+        "10",
+        "12",
+        "18",
+        "24",
+        "32",
+        "36",
     ]
     if is_npu()
     else []
@@ -81,15 +85,14 @@ class TestQwen3_235B(TestSingleMixUtils):
     other_args = QWEN3_235B_OTHER_ARGS
     envs = QWEN3_235B_ENVS
     dataset_name = "random"
-    max_concurrency = 576
-    num_prompts = int(max_concurrency) * 4
+    max_concurrency = 480
+    num_prompts = 480
     input_len = 2048
     output_len = 2048
     random_range_ratio = 1
     ttft = 10000
     tpot = 50
-    # PPU: 320@100ms.   800I: 1.8*PPU
-    output_token_throughput = 320 * 1.8 * 8 / 0.93
+    output_token_throughput = 8314
 
     def test_qwen3_235b(self):
         self.run_throughput()
