db9e573f85e88880245f91513551c7b3226a59b3
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-19 15:45:43 +0800

    Update test_ascend_deepseek_v3.2_w8a8_1p1d_in64k_out1k_30ms_tp64.py

--- test/srt/ascend_k8s/test_ascend_deepseek_v3.2_w8a8_1p1d_in64k_out1k_30ms_tp64.py
+++ test/srt/ascend_k8s/test_ascend_deepseek_v3.2_w8a8_1p1d_in64k_out1k_30ms_tp64.py
@@ -3,68 +3,72 @@ import unittest
 from test_ascend_single_mix_utils import NIC_NAME
 from test_ascend_disaggregation_utils import TestAscendDisaggregationUtils
 
-
-MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/Howeee/DeepSeek-R1-0528-w8a8"
+MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/DeepSeek-V3.2-Exp-W8A8"
 
 MODEL_CONFIG = {
     "model_path": MODEL_PATH,
     "prefill_envs": {
-        "SGLANG_SET_CPU_AFFINITY": "1",
+        "SGLANG_SET_CPU_AFFIMITY": "1",
+        "LD_LIBRARY_PATH": "/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}",
+        "PATH": "/usr/local/Ascend/8.5.0/compiler/bisheng/bin:$PATH",
+        "ASCEND_HOME_PATH": "/usr/local/Ascend/ascend-toolkit/latest",
         "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
         "STREAMS_PER_DEVICE": "32",
+        "HCCL_BUFFSIZE": "1024",
+        "DEEPEP_NORMAL_LONG_SEQ_ROUND": "5",
+        "DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS": "512",
         "SGLANG_NPU_USE_MLAPO": "1",
-        "SGLANG_USE_FIA_NZ": "1",
-        # "ENABLE_MOE_NZ": "1",
-        # "SGLANG_USE_AG_AFTER_QLORA": "1",
-        "HCCL_BUFFSIZE": "1536",
         "DEEP_NORMAL_MODE_USE_INT8_QUANT": "1",
-        "TASK_QUEUE_ENABLE": "2",
+        "SGLANG_NPU_USE_MULTI_STREAM": "1",
+        "HCCL_OP_EXPANSION_MODE": "AIV",
         "HCCL_SOCKET_IFNAME": NIC_NAME,
         "GLOO_SOCKET_IFNAME": NIC_NAME,
     },
     "decode_envs": {
-        "SGLANG_SET_CPU_AFFINITY": "1",
+        "SGLANG_SET_CPU_AFFIMITY": "1",
+        "LD_LIBRARY_PATH": "/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}",
+        "PATH": "/usr/local/Ascend/8.5.0/compiler/bisheng/bin:$PATH",
+        "ASCEND_HOME_PATH": "/usr/local/Ascend/ascend-toolkit/latest",
         "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
         "STREAMS_PER_DEVICE": "32",
+        "SGLANG_NPU_USE_MULTI_STREAM": "1",
         "SGLANG_NPU_USE_MLAPO": "1",
-        "SGLANG_USE_FIA_NZ": "1",
-        # "ENABLE_MOE_NZ": "1",
-        "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
-        "SGLANG_ENABLE_SPEC_V2": "1",
-        "HCCL_BUFFSIZE": "650",
-        "SGLANG_DP_ROUND_ROBIN": "1",
-        "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "12",
-        "TASK_QUEUE_ENABLE": "0",
+        "HCCL_OP_EXPANSION_MODE": "AIV",
         "SGLANG_SCHEDULER_SKIP_ALL_GATHER": "1",
+        "TASK_QUEUE_ENABLE": "0",
+        "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+        "SGLANG_ENABLE_SEPC_V2": "1",
         "HCCL_SOCKET_IFNAME": NIC_NAME,
-        "GLOO_SOCKET_IFNAME": NIC_NAME,    
+        "GLOO_SOCKET_IFNAME": NIC_NAME,
     },
     "prefill_args": [
         "--nnodes",
-        "1",
-        "--node-rank",
-        "0",
+        2,
         "--disaggregation-mode",
         "prefill",
-        "--tp-size",
-        16,
+        "--tp",
+        32,
+        "--watchdog-timeout",
+        9000,
         "--mem-fraction-static",
-        0.81,
-        "--quantization",
-        "modelslim",
-        "--max-running-requests",
-        4,
-        "--context-length",
-        8192,
+        0.73,
         "--disable-radix-cache",
         "--chunked-prefill-size",
         -1,
         "--max-prefill-tokens",
-        28680,
+        68000,
+        "--max-running-requests",
+        1,
         "--moe-a2a-backend",
         "deepep",
         "--deepep-mode",
         "normal",
+        "--quantization",
+        "modelslim",
+        "--disable-cuda-graph",
+        "--enable-nsa-prefill-context-parallel",
+        "--moe-dense-tp-size",    
+        1,
         "--speculative-algorithm",
         "NEXTN",
         "--speculative-num-steps",
@@ -73,45 +77,41 @@ MODEL_CONFIG = {
         1,
         "--speculative-num-draft-tokens",
         2,
-        "--dp-size",
-        2,
-        "--enable-dp-attention",
-        "--disable-shared-experts-fusion",
-        "--dtype",
-        "bfloat16",
-        "--enable-attn-tp-input-scattered",
     ],
     "decode_args": [
         "--nnodes",
         "2",
         "--disaggregation-mode",
         "decode",
-        "--tp-size",
+        "--tp",
         32,
-        "--dp-size",
-        16,
-        "--mem-fraction-static",
-        0.75,
-        "--max-running-requests",
+        "--dp",
+        8,
+        "--ep",
         32,
-        "--quantization",
-        "modelslim",
-        "--moe-a2a-backend",
-        "deepep",
+        "--moe-dense-tp-size",
+        1,
         "--enable-dp-attention",
-        "--deepep-mode",
-        "low_latency",
         "--enable-dp-lm-head",
-        "--moe-dense-tp",
-        "1",
-        "--cuda-graph-bs",
-        2,
-        4,
-        6,
         "--watchdog-timeout",
         9000,
-        "--context-length",
-        8192,
+        "--mem-fraction-static",
+        0.79,
+        "--disable-radix-cache",
+        "--chunked-prefill-size",
+        -1,
+        "--max-prefill-tokens",
+        68000,
+        "--max-running-requests",
+        "32",
+        "--cuda-graph-max-bs",
+        "4",
+        "--moe-a2a-backend",
+        "ascend_fuseep",
+        "--deepep-mode",
+        "low_latency",
+        "--quantization",
+        "modelslim",
         "--speculative-algorithm",
         "NEXTN",
         "--speculative-num-steps",
@@ -120,29 +120,24 @@ MODEL_CONFIG = {
         1,
         "--speculative-num-draft-tokens",
         4,
-        "--tokenizer-worker-num",
-        4,
-        "--prefill-round-robin-balance",
-        "--disable-shared-experts-fusion",
-        "--dtype",
-        "bfloat16",
+        "--prefill-round-robin-balance",  
+        "--load-balance-method",
+        "round_robin",
     ],
 }
 
-
-class Test_DeepSeek_R1_W8A8_2P1D(TestAscendDisaggregationUtils):
+class Test_DeepSeek_V3_2_W8A8_1P1D(TestAscendDisaggregationUtils):
     model_config = MODEL_CONFIG
     dataset_name = "random"
-    request_rate = 16
-    max_concurrency = 768
-    num_prompts = int(max_concurrency) * 4
-    input_len = 3500
-    output_len = 1500
+    request_rate = "inf"
+    max_concurrency = 1
+    num_prompts = 1
+    input_len = 64000
+    output_len = 1000
     random_range_ratio = 1
     ttft = 10000
-    tpot = 50
-    # T: 224@40ms    800I A3: 1.8*T
-    output_token_throughput = 224 * 1.8 * 32 / 0.93
+    tpot = 30
+    output_token_throughput = 1
 
     def test_throughput(self):
         self.run_throughput()
