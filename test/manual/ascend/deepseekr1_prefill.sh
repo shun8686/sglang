6f7719b8cd1bafff75457e7a1893b886248cb976
Cherry_ming <136634645@qq.com>
2025-12-24 21:02:49 +0800

    Update deepseekr1_prefill.sh

--- test/manual/ascend/deepseekr1_prefill.sh
+++ test/manual/ascend/deepseekr1_prefill.sh
@@ -15,27 +15,20 @@ sysctl -w kernel.numa_balancing=0
 sysctl -w kernel.sched_migration_cost_ns=50000
 
 export SGLANG_SET_CPU_AFFINITY=1
-
-export PYTHORCH_NPU_ALLOC_CONF=expandable_segments:True
-export STREAMS_PER_DEVICE=32
-
-export HCCL_SOCKET_TFNAME=enp23s0f3
-export GLOO_SOCKET_IFNAME=enp23s0f3
-
-export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=16
-export HCCL_BUFFSIZE=1600
-export HCCL_OP_EXPANSION_MODE=AIV
-export HCCL_ALGO="level0:NA;levrl1:ring"
-export SGLANG_SET_CPU_AFFINITY=1
 export PYTORCH_NPU_ALLOC_CONF="expandable_segments:True"
 export STREAMS_PER_DEVICE=32
 export SGLANG_NPU_USE_MLAPO=1
 export SGLANG_USE_FIA_NZ=1
 export ENABLE_MOE_NZ=1
+export HCCL_BUFFSIZE=1536
 export DEEP_NORMAL_MODE_USE_INT8_QUANT=1
 export TASK_QUEUE_ENABLE=2
+export HCCL_SOCKET_TFNAME=enp23s0f3
+export GLOO_SOCKET_IFNAME=enp23s0f3
+
 export PYTHONPATH=/usr/local/python3.11.13/lib/python3.11/site-packages/sglang:$PWD/python/:$PYTHONPATH
 export MODEL_PATH="/root/.cache/modelscope/hub/models/DeepSeek-R1-0528-w4a8"
+export logfile="./launch_decode_$(date +'%Y-%m-%d-%H:%M').log"
 
 
 # P节点
