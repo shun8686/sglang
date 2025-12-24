f79d58c00d67f01f893fbcd69cc7f8259eb30d61
Cherry_ming <136634645@qq.com>
2025-12-24 22:28:35 +0800

    Update deepseekr1_prefill.sh

--- test/manual/ascend/deepseekr1_prefill.sh
+++ test/manual/ascend/deepseekr1_prefill.sh
@@ -6,6 +6,7 @@ echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 sysctl -w vm.swappiness=0
 sysctl -w kernel.numa_balancing=0
 sysctl -w kernel.sched_migration_cost_ns=50000
+
 # 绑核
 export SGLANG_SET_CPU_AFFINITY=1
 
@@ -18,32 +19,39 @@ source /usr/local/Ascend/ascend-toolkit/set_env.sh
 source /usr/local/Ascend/nnal/atb/set_env.sh
 export PATH=/usr/local/Ascend/8.5.0/compiler/bishengir/bin:$PATH
 
+# 内存碎片
 export PYTORCH_NPU_ALLOC_CONF="expandable_segments:True"
 export STREAMS_PER_DEVICE=32
+# enable mlapo
 export SGLANG_NPU_USE_MLAPO=1
 export SGLANG_USE_FIA_NZ=1
 export ENABLE_MOE_NZ=1
+# prefill环境变量
 export HCCL_BUFFSIZE=1536
 export DEEP_NORMAL_MODE_USE_INT8_QUANT=1
 export TASK_QUEUE_ENABLE=2
 export HCCL_SOCKET_TFNAME=enp23s0f3
 export GLOO_SOCKET_IFNAME=enp23s0f3
 
-export PYTHONPATH=/usr/local/python3.11.13/lib/python3.11/site-packages/sglang:$PWD/python/:$PYTHONPATH
+# export PYTHONPATH=/usr/local/python3.11.13/lib/python3.11/site-packages/sglang:$PWD/python/:$PYTHONPATH
 export MODEL_PATH="/root/.cache/modelscope/hub/models/DeepSeek-R1-0528-w4a8"
 export logfile="./launch_prefill_$(date +'%Y-%m-%d-%H:%M').log"
 
 
 # P节点
+nohup \
 python -m sglang.launch_server --model-path ${MODEL_PATH} --disaggregation-mode prefill \
 --host 192.168.0.184 --port 8000 --disaggregation-bootstrap-port 8995 --trust-remote-code \
 --nnodes 1 \
 --node-rank 0 \
---disaggregation-mode prefill \
 --tp-size 16 \
---mem-fraction-static 0.8 \
+--mem-fraction-static 0.8 \  # 长序列增大至0.8
+--attention-backend ascend \
+--device npu \
 --quantization modelslim \
+--disaggregation-transfer-backend ascend \
 --max-running-requests 8 \
+# -context-length 8192  长序列场景不设置该参数
 --disable-radix-cache \
 --chunked-prefill-size -1 \
 --max-prefill-tokens 28680 \
@@ -57,3 +65,4 @@ python -m sglang.launch_server --model-path ${MODEL_PATH} --disaggregation-mode 
 --enable-dp-attention \
 --disable-shared-experts-fusion \
 --dtype bfloat16 \
+> $logfile 2>&1 & \
