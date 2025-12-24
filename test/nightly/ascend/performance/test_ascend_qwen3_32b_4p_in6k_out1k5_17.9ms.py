5521fd10b70c44f41fd432d94eefd3be69ce766e
cy <chenyang08056032@163.com>
2025-12-24 15:48:59 +0800

    modify output_token

--- test/nightly/ascend/performance/test_ascend_qwen3_32b_4p_in6k_out1k5_17.9ms.py
+++ test/nightly/ascend/performance/test_ascend_qwen3_32b_4p_in6k_out1k5_17.9ms.py
@@ -83,8 +83,8 @@ class TestQwen3_32B(TestSingleMixUtils):
     random_range_ratio = 1
     ttft = 10000
     tpot = 17.9
-    # T: 398@18ms. 800I A3: 1316.66
-    output_token_throughput = 1316.66 / 0.93
+    # T: 398@18ms. 800I A3: 1247.24
+    output_token_throughput = 1247.24 / 0.93
 
     def test_qwen3_32b(self):
         self.run_throughput()
