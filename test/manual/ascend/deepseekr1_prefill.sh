61ff4795f5d500c905e87f5e7eb961f83c1954b6
Cherry_ming <136634645@qq.com>
2025-12-25 23:27:24 +0800

    Update deepseekr1_prefill.sh

--- test/manual/ascend/deepseekr1_prefill.sh
+++ test/manual/ascend/deepseekr1_prefill.sh
@@ -23,7 +23,7 @@ export PATH=/usr/local/Ascend/8.5.0/compiler/bishengir/bin:$PATH
 export PYTORCH_NPU_ALLOC_CONF="expandable_segments:True"
 export STREAMS_PER_DEVICE=32
 # pd传输, IP设置为p节点首节点
-export ASCEND_MF_STORE_URL="tcp://192.168.0.184:24667"
+export ASCEND_MF_STORE_URL="tcp://141.61.39.231:24667"
 # enable mlapo
 export SGLANG_NPU_USE_MLAPO=1
 export SGLANG_USE_FIA_NZ=1
