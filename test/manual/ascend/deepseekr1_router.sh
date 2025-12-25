6d48eb65403252c8b61781f122b8ade5beb0be22
Cherry_ming <136634645@qq.com>
2025-12-25 23:34:25 +0800

    Update deepseekr1_router.sh

--- test/manual/ascend/deepseekr1_router.sh
+++ test/manual/ascend/deepseekr1_router.sh
@@ -5,6 +5,6 @@ python -u -m sglang_router.launch_router \
     --pd-disaggregation \
     --host 127.0.0.1 \
     --port 6688 \
-    --prefill http://192.168.0.184:8000 8995\
-    --decode http://192.168.0.60:8001 \
+    --prefill http://141.61.39.231:8000 8995\
+    --decode http://141.61.29.201:8001 \
 > $logfile 2>&1 & \
