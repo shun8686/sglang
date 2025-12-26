5f8bd8bdff1145bdedb42539d7bbaabdc24eea02
Cherry_ming <136634645@qq.com>
2025-12-26 09:32:43 +0800

    Update test_ascend_lts_deepseek_r1_v1.py

--- test/manual/ascend/test_ascend_lts_deepseek_r1_v1.py
+++ test/manual/ascend/test_ascend_lts_deepseek_r1_v1.py
@@ -18,7 +18,7 @@ from sglang.test.test_utils import (
 
 
 # MODEL_PATH = "/home/weights/DeepSeek-R1_w8a8"
-MODEL_PATH = "/root/.cache/modelscope/hub/models/DeepSeek-R1-0528-w4a8"
+MODEL_PATH = "/root/.cache/modelscope/hub/models/DeepSeek-R1-0528-w4a8-per-channel/"
 
 
 def run_command(cmd, shell=True):
