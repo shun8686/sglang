56ae5eb6389c4b7a55534880b304a8612d1ccbb5
shun8686 <verve@vip.qq.com>
2025-12-31 12:05:58 +0800

    update

--- test/nightly/ascend/performance/test_ascend_deepseek_r1_w8a8_2p1d_32p_in3k9_out1k_20ms.py
+++ test/nightly/ascend/performance/test_ascend_deepseek_r1_w8a8_2p1d_32p_in3k9_out1k_20ms.py
@@ -141,8 +141,8 @@ class Test_DeepSeek_R1_W8A8_2P1D_In3900_Out1024(TestAscendDisaggregationUtils):
     model_config = MODEL_CONFIG
     dataset_name = "random"
     request_rate = 16
-    max_concurrency = 32
-    num_prompts = 32
+    max_concurrency = 768
+    num_prompts = 768
     input_len = 3900
     output_len = 1024
     random_range_ratio = 1
