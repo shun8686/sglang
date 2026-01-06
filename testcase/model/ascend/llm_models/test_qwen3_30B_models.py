43cb58c7648fbc1bd93ae70060b57f6f0fa905f5
chenyang08056032 <chenyang08056032@163.com>
2026-01-06 15:09:23 +0800

    Update test_qwen3_30B_models.py

--- testcase/model/ascend/llm_models/test_qwen3_30B_models.py
+++ testcase/model/ascend/llm_models/test_qwen3_30B_models.py
@@ -47,7 +47,7 @@ class TestAscendTp4Bf16(CustomTestCase):
         ]
         
         cls.process = popen_launch_server(
-            cls.model,
+            cls.models,
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=cls.common_args,
