7b4e06272393b0cb418d93f34ef447b9d45824e3
chenyang08056032 <chenyang08056032@163.com>
2026-01-08 17:08:48 +0800

    Update test_ascend_qwen3_235b_a22b_w8a8.py

--- testcase/model/ascend/llm_models/test_ascend_qwen3_235b_a22b_w8a8.py
+++ testcase/model/ascend/llm_models/test_ascend_qwen3_235b_a22b_w8a8.py
@@ -10,6 +10,18 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/vllm-ascend/Qwen3-235B-A22B-W8A8"
     accuracy = 0
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        "4",
+        "--quantization",
+        "modelslim",
+    ]
 
 
 if __name__ == "__main__":
