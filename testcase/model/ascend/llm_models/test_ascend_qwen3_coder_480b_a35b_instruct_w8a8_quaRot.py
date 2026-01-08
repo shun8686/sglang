6b22342c167479dfe1d2549bf468f97b78b439cf
chenyang08056032 <chenyang08056032@163.com>
2026-01-08 16:22:05 +0800

    Update test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quaRot.py

--- testcase/model/ascend/llm_models/test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quaRot.py
+++ testcase/model/ascend/llm_models/test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quaRot.py
@@ -13,7 +13,7 @@ class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     other_args = [
         "--trust-remote-code",
         "--mem-fraction-static",
-        "0.5",
+        "0.95",
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
