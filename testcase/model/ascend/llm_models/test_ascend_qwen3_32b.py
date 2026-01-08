0f7561fda55d64fdddfbb28662cb8d5ec2b1f5ce
chenyang08056032 <chenyang08056032@163.com>
2026-01-08 15:51:58 +0800

    Update test_ascend_qwen3_32b.py

--- testcase/model/ascend/llm_models/test_ascend_qwen3_32b.py
+++ testcase/model/ascend/llm_models/test_ascend_qwen3_32b.py
@@ -9,7 +9,7 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/Qwen/Qwen3-32B"
-    accuracy = 0
+    accuracy = 0.89
     other_args = [
         "--trust-remote-code",
         "--mem-fraction-static",
