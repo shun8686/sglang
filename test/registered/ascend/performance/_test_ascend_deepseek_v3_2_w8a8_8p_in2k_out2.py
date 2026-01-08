cbf853bc995665d3a4a2b3f850bb8f2f159e0ef2
shun8686 <verve@vip.qq.com>
2026-01-08 14:57:58 +0800

    Update testcases.

---
+++ test/registered/ascend/performance/_test_ascend_deepseek_v3_2_w8a8_8p_in2k_out2.py
@@ -0,0 +1,64 @@
+import unittest
+
+from test_ascend_single_mix_utils import TestSingleNodeTestCaseBase, NIC_NAME
+
+MODEL_PATH = "/root/.cache/modelscope/hub/models/DeepSeek-V3.2-Exp-W8A8"
+
+ENVS = {
+    # "SGLANG_SET_CPU_AFFINITY": "1",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "STREAMS_PER_DEVICE": "32",
+    "HCCL_BUFFSIZE": "1024",
+    "DEEPEP_NORMAL_LONG_SEQ_ROUND": "5",
+    "DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS": "512",
+    "SGLANG_NPU_USE_MLAPO": "1",
+    "DEEP_NORMAL_MODE_USE_INT8_QUANT": "1",
+    "SGLANG_NPU_USE_MULTI_STREAM": "1",
+    "HCCL_OP_EXPANSION_MODE": "AIV",
+    "HCCL_SOCKET_IFNAME": NIC_NAME,
+    "GLOO_SOCKET_IFNAME": NIC_NAME,
+    "SGLANG_SCHEDULER_SKIP_ALL_GATHER": "1",
+    "TASK_QUEUE_ENABLE": "0",
+    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+    "SGLANG_ENABLE_SPEC_V2": "1",
+    "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "8",
+}
+
+OTHER_ARGS = (
+    [
+        "--tp-size", "16",
+        "--trust-remote-code",
+        "--attention-backend", "ascend",
+        "--device", "npu",
+        "--quantization", "modelslim",
+        "--mem-fraction-static", "0.8",
+        "--chunked-prefill-size", "8192"
+        "--context-length", "40970",
+        "--max-prefill-tokens", "40970",
+        "--max-total-tokens", "40970",
+        "--watchdog-timeout", "9000",
+        "--disable-radix-cache",
+        "--max-running-requests", 32,
+        "--cuda-graph-bs", 1, 2, 4, 8, 16, 32,
+    ]
+)
+
+class TestDeepSeekV32(TestSingleNodeTestCaseBase):
+    model = MODEL_PATH
+    other_args = OTHER_ARGS
+    envs = ENVS
+    dataset_name = "random"
+    max_concurrency = 32
+    num_prompts = 320
+    input_len = 2048
+    output_len = 2048
+    random_range_ratio = 1
+    tpot = 50
+    output_token_throughput = 5000
+
+    def test_qwen3_235b(self):
+        self.run_throughput()
+
+
+if __name__ == "__main__":
+    unittest.main()
