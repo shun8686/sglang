134ae8c8bf227acc8e75e6d561e41eb952bb2b81
cy <chenyang08056032@163.com>
2026-01-09 13:22:48 +0800

    add

--- testcase/model/ascend/llm_models/test_ascend_dbrx_instruct.py
+++ testcase/model/ascend/llm_models/test_ascend_dbrx_instruct.py
@@ -10,7 +10,16 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/AI-ModelScope/dbrx-instruct"
     accuracy = 0
-
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        "2",
+    ]
 
 if __name__ == "__main__":
     unittest.main()
