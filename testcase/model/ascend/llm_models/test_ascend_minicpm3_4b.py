15846db9a1f7bbac82ec3ef336057a8fd3fc8c6d
cy <chenyang08056032@163.com>
2026-01-09 13:06:49 +0800

    add

--- testcase/model/ascend/llm_models/test_ascend_minicpm3_4b.py
+++ testcase/model/ascend/llm_models/test_ascend_minicpm3_4b.py
@@ -10,7 +10,16 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 class TestMiniCPM3(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/OpenBMB/MiniCPM3-4B"
     accuracy = 0
-
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--disable-radix-cache",
+        "--disable-overlap-schedule",
+    ]
 
 if __name__ == "__main__":
     unittest.main()
