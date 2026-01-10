e633fbb8a29859c44c165ff33d162c38601b45e7
yangfen0903 <2253770994@qq.com>
2026-01-10 17:06:54 +0800

    add

---
+++ testcase/api/test_ascend_api_v1_completions.py
@@ -0,0 +1,216 @@
+import requests
+import unittest
+from sglang.srt.utils import kill_process_tree
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+
+class TestEnableThinking(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "/root/.cache/modelscope/hub/models/Qwen/Qwen3-30B-A3B"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.api_key = "sk-1234"
+        cls.other_args = [
+            "--reasoning-parser",
+            "qwen3",
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--mem-fraction-static",
+            0.8,
+            "--tp-size",
+            2,
+            "--base-gpu-id",
+            "6"
+        ]
+
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            api_key=cls.api_key,
+            other_args=cls.other_args,
+        )
+        cls.additional_chat_kwargs = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_model_parameters_model(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "model": self.model,
+                "prompt": 'who are you?'
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_prompt(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?'
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+        # client = requests.post(
+        #     f"{self.base_url}/v1/completions",
+        #     json={
+        #         "model": self.model,
+        #         "messages": [{"role": "user", "content": "Hello"}],
+        #         "temperature": 0,
+        #         "frequency_penalty": 1,
+        #     },
+        # )
+        # print(f"client:{client}")
+        # print(f"client.status_code:{client.status_code}")
+        # print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        # self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+
+    def test_model_parameters_max_tokens(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                'max_tokens': 512
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_stream(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                "stream": True
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_temperature(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                "temperature": 0
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_hidden_states(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                "return_hidden_status": True
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_top_k(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                "top_k": 1
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_stop_token_ids(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                "stop_token_ids": 1
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+    def test_model_parameters_rid(self):
+        client = requests.post(
+            f"{self.base_url}/v1/completions",
+            json={
+                "prompt": 'who are you?',
+                "rid": "10086"
+            },
+        )
+        print(f"client:{client}")
+        print(f"client.status_code:{client.status_code}")
+        print(f"client.json:{client.json()}")
+        # print(f"client.text:{client.text}")
+        self.assertEqual(client.status_code, 200, f"Failed with: {client.text}")
+        # data = client.json()
+        # self.assertEqual(data["model"], self.model)
+        # self.assertIsNotNone(data["choices"][0]["message"]["reasoning_content"])
+
+
+
+if __name__ == "__main__":
+    unittest.main()
