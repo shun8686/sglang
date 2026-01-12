25104a07f8cedb35d9934e8f304fed32658b7ac2
shun8686 <verve@vip.qq.com>
2026-01-12 18:16:32 +0800

    Update testcases.

---
+++ test/registered/ascend/features/deepep/fused_deep_moe/test_deepep_fused_deep_moe_deepseek_v2.py
@@ -0,0 +1,67 @@
+import json
+import os
+import unittest
+from types import SimpleNamespace
+
+from sglang.srt.environ import envs
+from sglang.srt.utils import kill_process_tree
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+MODEL_PATH = "/root/.cache/modelscope/hub/models/deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct/"
+
+class TestPureTP(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = MODEL_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--trust-remote-code",
+                "--tp-size",
+                "2",
+                "--dp-size",
+                "2",
+                "--enable-dp-attention",
+                "--moe-a2a-backend",
+                "ascend_fuseep",
+                "--disable-cuda-graph",
+            ],
+            env={
+                "SGLANG_ENABLE_JIT_DEEPGEMM": "0",
+                "SGLANG_EXPERT_LOCATION_UPDATER_CANARY": "1",
+                "SGLANG_DEEPEP_BF16_DISPATCH": "1",
+                "HCCL_BUFFSIZE": "1024",
+                "MOE_ENABLE_TOPK_NEG_ONE": "1",
+                **os.environ,
+            },
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_mmlu(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="mmlu",
+            num_examples=64,
+            num_threads=32,
+        )
+
+        metrics = run_eval(args)
+        self.assertGreater(metrics["score"], 0.5)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
