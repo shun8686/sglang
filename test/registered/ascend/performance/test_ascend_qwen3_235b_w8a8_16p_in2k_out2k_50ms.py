1cf3695a639d00be548ef1a04384ec0f1931de83
shun8686 <verve@vip.qq.com>
2026-01-14 11:38:11 +0800

    Update testcases.

--- test/registered/ascend/performance/test_ascend_qwen3_235b_w8a8_16p_in2k_out2k_50ms.py
+++ test/registered/ascend/performance/test_ascend_qwen3_235b_w8a8_16p_in2k_out2k_50ms.py
@@ -19,9 +19,7 @@ MODEL_CONFIG = {
         "HCCL_OP_EXPANSION_MODE": "AIV",
         "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
         "SGLANG_ENABLE_SPEC_V2": "1",
-        # "DISABLE_EAGLE3_QUANT": "1",
-        "SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE": "1",
-        # "ENABLE_PROFILING": "1",
+        # "SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE": "1",
     },
     "other_args": [
         "--trust-remote-code",
