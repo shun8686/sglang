13f0569589497e13b14f8486c61aeb3465b4c9fa
cy <chenyang08056032@163.com>
2026-01-14 16:32:29 +0800

    add

--- testcase/model/ascend/reward_models/test_ascend_qwen2_5_math_rm_72b.py
+++ testcase/model/ascend/reward_models/test_ascend_qwen2_5_math_rm_72b.py
@@ -33,7 +33,7 @@ class TestInternlm2_7bReward(CustomTestCase):
     #        "/data/l30079981/weights/Qwen2.5-Math-RM-72B"
     #
  #   )
-    model_path="/root/.cache/modelscope/hub/models/Howeee/Qwen2.5-Math-RM-72B"
+    model_path="/root/.cache/modelscope/hub/models/Qwen/Qwen2.5-Math-RM-72B"
     torch_dtype = torch.float16
     tolerance = 4e-2
     tp_size = 4
