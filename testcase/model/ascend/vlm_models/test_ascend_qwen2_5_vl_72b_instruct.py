1eb29d07ade1c51ee1a53bba78b8d83163731b2c
cy <chenyang08056032@163.com>
2026-01-14 17:04:14 +0800

    add

--- testcase/model/ascend/vlm_models/test_ascend_qwen2_5_vl_72b_instruct.py
+++ testcase/model/ascend/vlm_models/test_ascend_qwen2_5_vl_72b_instruct.py
@@ -9,6 +9,21 @@ register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
 class TestGemmaModels(TestVLMModels):
     model = "/root/.cache/modelscope/hub/models/Qwen/Qwen2.5-VL-72B-Instruct"
     mmmu_accuracy = 0.56
+    other_args = [
+        "--trust-remote-code",
+        "--cuda-graph-max-bs",
+        "32",
+        "--enable-multimodal",
+        "--mem-fraction-static",
+        0.8,
+        "--log-level",
+        "info",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        4,
+    ]
 
     def test_vlm_mmmu_benchmark(self):
         self._run_vlm_mmmu_test()
