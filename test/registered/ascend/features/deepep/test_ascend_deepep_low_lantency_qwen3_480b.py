a82f15a182531288fb4c698e5fafa8f2e40c0ba6
shun8686 <verve@vip.qq.com>
2026-01-15 22:21:01 +0800

    Update testcases.

--- test/registered/ascend/features/deepep/test_ascend_deepep_low_lantency_qwen3_480b.py
+++ test/registered/ascend/features/deepep/test_ascend_deepep_low_lantency_qwen3_480b.py
@@ -33,7 +33,7 @@ class TestDeepEpQwen(CustomTestCase):
                 "--max-running-requests", 96,
                 "--context-length", 8192,
                 "--dtype", "bfloat16",
-                "--chunked-prefill-size", 28672,
+                "--chunked-prefill-size", 8192,
                 "--max-prefill-tokens", 458880,
                 "--disable-radix-cache",
                 "--moe-a2a-backend", "deepep",
