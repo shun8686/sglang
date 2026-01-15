5901639e7f54d27175bf979eb5e3a43ca485f9c0
cy <chenyang08056032@163.com>
2026-01-15 16:34:45 +0800

    add

--- testcase/model/ascend/llm_models/test_ascend_deepseek_v3_2_exp_w8a8.py
+++ testcase/model/ascend/llm_models/test_ascend_deepseek_v3_2_exp_w8a8.py
@@ -9,7 +9,7 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/DeepSeek-V3.2-Exp-W8A8"
-    accuracy = 0
+    accuracy = 0.565
     other_args = [
         "--trust-remote-code",
         "--mem-fraction-static",
