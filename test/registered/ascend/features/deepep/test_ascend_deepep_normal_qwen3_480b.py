59db52eba1484ff798bbd953dcd59b89bcdcb2d6
shun8686 <verve@vip.qq.com>
2026-01-15 21:03:06 +0800

    Update testcases.

--- test/registered/ascend/features/deepep/test_ascend_deepep_normal_qwen3_480b.py
+++ test/registered/ascend/features/deepep/test_ascend_deepep_normal_qwen3_480b.py
@@ -5,6 +5,7 @@ from types import SimpleNamespace
 from utils.test_ascend_deepep_mode_config import QWEN3_CODER_480B_A35B_W8A8_MODEL_PATH, NIC_NAME
 from sglang.srt.utils import kill_process_tree
 from sglang.test.run_eval import run_eval
+from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -83,7 +84,7 @@ class TestDeepEpQwen(CustomTestCase):
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
         )
-        metrics = run_eval(args)
+        metrics = run_gsm8k(args)
         self.assertGreaterEqual(
             metrics["accuracy"],
             expect_accuracy,
