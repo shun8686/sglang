6fc48f50f47c1b714d89e25a837a5f0d590fdf1b
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-16 16:54:15 +0800

    Update test_ascend_deepep_low_latency_deepseek_v3_2_exp_w8a8.py

--- test/registered/ascend/features/deepep/test_ascend_deepep_low_latency_deepseek_v3_2_exp_w8a8.py
+++ test/registered/ascend/features/deepep/test_ascend_deepep_low_latency_deepseek_v3_2_exp_w8a8.py
@@ -2,14 +2,17 @@ import os
 import unittest
 from types import SimpleNamespace
 
+from utils.test_ascend_deepep_mode_config import NIC_NAME
 from sglang.srt.utils import kill_process_tree
 from sglang.test.run_eval import run_eval
+from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
 )
+
 
 
 class TestDeepEpDeepseek(CustomTestCase):
