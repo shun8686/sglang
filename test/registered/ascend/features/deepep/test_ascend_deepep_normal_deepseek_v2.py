9b3affbdf0987e97b3bfd31e5a5bed2337ccf163
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-16 14:36:43 +0800

    Update test_ascend_deepep_normal_deepseek_v2.py

--- test/registered/ascend/features/deepep/test_ascend_deepep_normal_deepseek_v2.py
+++ test/registered/ascend/features/deepep/test_ascend_deepep_normal_deepseek_v2.py
@@ -1,18 +1,4 @@
-import os
-import unittest
-from types import SimpleNamespace
-
-from utils.test_ascend_deepep_mode_config import DEEPSEEK_CODER_V2_LITE_MODEL_PATH
-from sglang.srt.utils import kill_process_tree
-from sglang.test.run_eval import run_eval
-from sglang.test.test_utils import (
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    DEFAULT_URL_FOR_TEST,
-    CustomTestCase,
-    popen_launch_server,
-)
-
-class TestPureTP(CustomTestCase):
+class TestDeepEpDeepseek(CustomTestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = DEEPSEEK_CODER_V2_LITE_MODEL_PATH
@@ -23,19 +9,25 @@ class TestPureTP(CustomTestCase):
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=[
                 "--trust-remote-code",
+                "--attention-backend", "ascend",
                 "--tp-size",
-                "2",
+                "8",
                 "--moe-a2a-backend",
                 "deepep",
                 "--deepep-mode",
                 "normal",
+                "--mem-fraction-static", 0.7,
                 "--disable-cuda-graph",
+                "--dtype", "bfloat16",
+                "--disable-radix-cache",
             ],
             env={
-                "SGLANG_ENABLE_JIT_DEEPGEMM": "0",
-                "SGLANG_EXPERT_LOCATION_UPDATER_CANARY": "1",
+                "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+                "SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT": "600",
                 "HCCL_BUFFSIZE": "2048",
-                "MOE_ENABLE_TOPK_NEG_ONE": "1",
+                "HCCL_SOCKET_IFNAME": NIC_NAME,
+                "GLOO_SOCKET_IFNAME": NIC_NAME,
+                "HCCL_OP_EXPANSION_MODE": "AIV",
                 **os.environ,
             },
         )
@@ -45,6 +37,7 @@ class TestPureTP(CustomTestCase):
         kill_process_tree(cls.process.pid)
 
     def test_mmlu(self):
+        expect_score = 0.85
         args = SimpleNamespace(
             base_url=self.base_url,
             model=self.model,
@@ -52,9 +45,28 @@ class TestPureTP(CustomTestCase):
             num_examples=8,
             num_threads=32,
         )
-
+        print("Starting mmlu test...")
         metrics = run_eval(args)
-        self.assertGreater(metrics["score"], 0.5)
+        self.assertGreater(metrics["score"], expect_score)
+
+    def test_gsm8k(self):
+        expect_accuracy = 0.85
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        print("Starting gsm8k test...")
+        metrics = run_gsm8k(args)
+        self.assertGreaterEqual(
+            metrics["accuracy"],
+            expect_accuracy,
+            f'Accuracy of {self.model} is {str(metrics["accuracy"])}, is lower than {expect_accuracy}',
+        )
 
 
 if __name__ == "__main__":
