ba42e307ae60a4f188aad92efcc5b9199464a8fa
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-16 16:13:47 +0800

    Update test_ascend_deepep_normal_deepseek_v2.py

--- test/registered/ascend/features/deepep/test_ascend_deepep_normal_deepseek_v2.py
+++ test/registered/ascend/features/deepep/test_ascend_deepep_normal_deepseek_v2.py
@@ -32,10 +32,15 @@ class TestDeepEpDeepseek(CustomTestCase):
                 "deepep",
                 "--deepep-mode",
                 "normal",
+                "--max-running-requests", 96,
                 "--mem-fraction-static", 0.7,
                 "--disable-cuda-graph",
                 "--dtype", "bfloat16",
                 "--disable-radix-cache",
+                "--dp-size", 8,
+                "--enable-dp-attention",
+                "--enable-dp-lm-head",
+                "--chunked-prefill-size", 1024,
             ],
             env={
                 "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
@@ -44,6 +49,7 @@ class TestDeepEpDeepseek(CustomTestCase):
                 "HCCL_SOCKET_IFNAME": NIC_NAME,
                 "GLOO_SOCKET_IFNAME": NIC_NAME,
                 "HCCL_OP_EXPANSION_MODE": "AIV",
+                "SGLANG_DEEPEP_BF16_DISPATCH": "1",
                 **os.environ,
             },
         )
