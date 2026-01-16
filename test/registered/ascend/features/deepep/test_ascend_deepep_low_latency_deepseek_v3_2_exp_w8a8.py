7e6f3a66c5cababf320d479e72d3c8c58f66537d
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-16 16:30:14 +0800

    Update test_ascend_deepep_low_latency_deepseek_v3_2_exp_w8a8.py

--- test/registered/ascend/features/deepep/test_ascend_deepep_low_latency_deepseek_v3_2_exp_w8a8.py
+++ test/registered/ascend/features/deepep/test_ascend_deepep_low_latency_deepseek_v3_2_exp_w8a8.py
@@ -47,7 +47,7 @@ class TestDeepEpDeepseek(CustomTestCase):
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
 
-     def test_mmlu(self):
+    def test_mmlu(self):
         expect_score = 0.565
         args = SimpleNamespace(
             base_url=self.base_url,
