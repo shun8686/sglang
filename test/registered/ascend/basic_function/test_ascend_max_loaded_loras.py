b5c7411b5a6a4b6a081193f5fc08fb8e5ec8151b
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-20 10:26:57 +0800

    Update test_ascend_max_loaded_loras.py

--- test/registered/ascend/basic_function/test_ascend_max_loaded_loras.py
+++ test/registered/ascend/basic_function/test_ascend_max_loaded_loras.py
@@ -11,6 +11,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
+register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 class TestMaxLoadedLoras(CustomTestCase):
     def test_max_loaded_loras(self):
