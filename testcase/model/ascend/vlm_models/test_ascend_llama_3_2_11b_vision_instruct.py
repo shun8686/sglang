a47b6593caf1ffd0eabb371bf24a358c54fbe35f
cy <chenyang08056032@163.com>
2026-01-22 18:07:11 +0800

    adapt

--- testcase/model/ascend/vlm_models/test_ascend_llama_3_2_11b_vision_instruct.py
+++ testcase/model/ascend/vlm_models/test_ascend_llama_3_2_11b_vision_instruct.py
@@ -11,6 +11,15 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-11B-Vision-Instruct"
     accuracy = 0
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--disable-radix-cache",
+    ]
 
 
 if __name__ == "__main__":
