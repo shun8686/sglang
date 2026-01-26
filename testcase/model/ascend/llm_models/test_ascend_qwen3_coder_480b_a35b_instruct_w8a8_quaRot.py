b7adb6d7688fb4fae7f3c12bf5cb6cdcfa4b4f0b
cy <chenyang08056032@163.com>
2026-01-26 10:32:23 +0800

    add

--- testcase/model/ascend/llm_models/test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quaRot.py
+++ testcase/model/ascend/llm_models/test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quaRot.py
@@ -9,7 +9,7 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/Qwen3-Coder-480B-A35B-Instruct-w8a8-QuaRot"
-    accuracy = 0.96
+    accuracy = 0.94
     timeout=3000
     other_args = [
         "--trust-remote-code",
