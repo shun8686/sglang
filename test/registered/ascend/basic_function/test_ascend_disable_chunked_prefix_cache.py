c7a1337afa3079c9b1b9cb8f0040321676704570
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-27 16:49:06 +0800

    Update test_ascend_disable_chunked_prefix_cache.py

--- test/registered/ascend/basic_function/test_ascend_disable_chunked_prefix_cache.py
+++ test/registered/ascend/basic_function/test_ascend_disable_chunked_prefix_cache.py
@@ -12,6 +12,7 @@ from sglang.test.test_utils import (
     CustomTestCase,
     popen_launch_server,
 )
+from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=400, suite="nightly-16-npu-a3", nightly=True)
 
