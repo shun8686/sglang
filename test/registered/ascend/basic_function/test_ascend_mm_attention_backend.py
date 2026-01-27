614ea7982b47383bcf41f1e361b659b48f339f2d
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-27 16:50:10 +0800

    Create test_ascend_mm_attention_backend.py

---
+++ test/registered/ascend/basic_function/test_ascend_mm_attention_backend.py
@@ -0,0 +1,35 @@
+import unittest
+
+from sglang.test.ascend.vlm_utils import TestVLMModels
+from sglang.test.ci.ci_register import register_npu_ci
+
+register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
+
+
+class TestGemmaModels(TestVLMModels):
+    model = "/root/.cache/modelscope/hub/models/google/gemma-3-4b-it"
+    mmmu_accuracy = 0.2
+    other_args = [
+        "--trust-remote-code",
+        "--cuda-graph-max-bs",
+        "32",
+        "--enable-multimodal",
+        "--mem-fraction-static",
+        0.35,
+        "--log-level",
+        "info",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        4,
+        "--mm-attention-backend",
+        "ascend_attn",
+    ]
+
+    def test_vlm_mmmu_benchmark(self):
+        self._run_vlm_mmmu_test()
+
+
+if __name__ == "__main__":
+    unittest.main()
