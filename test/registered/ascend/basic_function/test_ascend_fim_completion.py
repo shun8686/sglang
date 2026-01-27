19034a9baf73850bc1a16a4dd70238b42d1ec455
cy <chenyang08056032@163.com>
2026-01-27 17:09:52 +0800

    add

--- test/registered/ascend/basic_function/test_ascend_fim_completion.py
+++ test/registered/ascend/basic_function/test_ascend_fim_completion.py
@@ -3,6 +3,7 @@ import unittest
 import openai
 
 from sglang.srt.utils import kill_process_tree
+from sglang.test.ci.ci_register import register_npu_ci
 from sglang.srt.utils.hf_transformers_utils import get_tokenizer
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -10,6 +11,8 @@ from sglang.test.test_utils import (
     CustomTestCase,
     popen_launch_server,
 )
+
+register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestFimCompletion(CustomTestCase):
