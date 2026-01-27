bd4c9aec29dfc68696ec315f27e07165ae68c47d
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-27 15:31:26 +0800

    Update test_reasoning_parser.py

--- test/registered/ascend/basic_function/test_reasoning_parser.py
+++ test/registered/ascend/basic_function/test_reasoning_parser.py
@@ -9,6 +9,7 @@ from sglang.srt.parser.reasoning_parser import (
     StreamingParseResult,
 )
 from sglang.test.test_utils import CustomTestCase
+from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
