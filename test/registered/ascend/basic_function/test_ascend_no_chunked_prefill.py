c72c8fa2ee68cf3588c10b142d78b55a03a7b859
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-28 16:41:04 +0800

    Update test_ascend_no_chunked_prefill.py

--- test/registered/ascend/basic_function/test_ascend_no_chunked_prefill.py
+++ test/registered/ascend/basic_function/test_ascend_no_chunked_prefill.py
@@ -1,12 +1,10 @@
 import unittest
 
-from sglang.srt.utils import is_npu
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_MODEL_NAME_FOR_TEST,
     CustomTestCase,
     run_bench_serving,
-    run_mmlu_test,
 )
 
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
@@ -19,16 +17,9 @@ class TestNoChunkedPrefill(CustomTestCase):
     [Test Target] --disable-radix-cache
     """
 
-    def _no_chunked_prefill(self):
-        run_mmlu_test(
-            disable_radix_cache=False, enable_mixed_chunk=False, chunked_prefill_size=-1
-        )
-
     def test_no_chunked_prefill_without_radix_cache(self):
         model = (
             "/root/.cache/modelscope/hub/models/AI-ModelScope/Llama-3.1-8B-Instruct"
-            if is_npu()
-            else DEFAULT_MODEL_NAME_FOR_TEST
         )
         other_args = (
             [
@@ -41,8 +32,6 @@ class TestNoChunkedPrefill(CustomTestCase):
                 "--base-gpu-id",
                 2,
             ]
-            if is_npu()
-            else ["--disable-radix-cache", "--chunked-prefill-size", "-1"]
         )
         res = run_bench_serving(
             model=model,
