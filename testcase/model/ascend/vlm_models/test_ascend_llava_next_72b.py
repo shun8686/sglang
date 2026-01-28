ec8d8a739ff5b1e89fdc1046476b85e3ba1e7345
cy <chenyang08056032@163.com>
2026-01-28 11:25:19 +0800

    adapt

--- testcase/model/ascend/vlm_models/test_ascend_llava_next_72b.py
+++ testcase/model/ascend/vlm_models/test_ascend_llava_next_72b.py
@@ -2,25 +2,25 @@ import unittest
 
 from gsm8k_ascend_mixin import GSM8KAscendMixin
 
-#from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
-#register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/lmms-lab/llava-next-72b"
     accuracy = 0.79
     other_args = [
-            "--trust-remote-code",
-            "--tp-size",
-            16,
-            "--mem-fraction-static",
-            "0.8",
-            "--attention-backend",
-            "ascend",
-            "--disable-cuda-graph",
-            "--enable-multimodal",
+        "--trust-remote-code",
+        "--tp-size",
+        16,
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--enable-multimodal",
     ]
 
 
--- testcase/model/ascend/vlm_models/test_ascend_llava_v1_6_34b.py
+++ testcase/model/ascend/vlm_models/test_ascend_llava_v1_6_34b.py
@@ -2,31 +2,31 @@ import unittest
 
 from gsm8k_ascend_mixin import GSM8KAscendMixin
 
-#from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
-#register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
 
 
 class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
     model = "/root/.cache/modelscope/hub/models/AI-ModelScope/llava-v1.6-34b"
     accuracy = 0.63
     other_args = [
-            "--trust-remote-code",
-            "--tp-size",
-            4,
-            "--max-running-requests",
-            2048,
-            "--mem-fraction-static",
-            "0.7",
-            "--attention-backend",
-            "ascend",
-            "--disable-cuda-graph",
-            "--mm-per-request-timeout",
-            60,
-            "--enable-multimodal",
-            "--tokenizer-path",
-            "/data/c30044170/code/newHDK/llava-1.6v-34b-tokenizer"
+        "--trust-remote-code",
+        "--tp-size",
+        4,
+        "--max-running-requests",
+        2048,
+        "--mem-fraction-static",
+        "0.7",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--mm-per-request-timeout",
+        60,
+        "--enable-multimodal",
+        "--tokenizer-path",
+        "/data/c30044170/code/newHDK/llava-1.6v-34b-tokenizer"
     ]
 
 
