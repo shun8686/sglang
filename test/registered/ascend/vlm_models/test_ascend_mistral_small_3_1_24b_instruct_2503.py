5884c25b19a2e5bac60739da07620fdbf78ebf6f
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 23:21:16 +0800

    Update test_ascend_mistral_small_3_1_24b_instruct_2503.py

--- test/registered/ascend/vlm_models/test_ascend_mistral_small_3_1_24b_instruct_2503.py
+++ test/registered/ascend/vlm_models/test_ascend_mistral_small_3_1_24b_instruct_2503.py
@@ -8,7 +8,7 @@ register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
 
 
 class TestMistralModels(TestVLMModels):
-    """Testcase:Test the accuracy of the mistralai/Mistral-Small-3.1-24B-Instruct-2503 model using the mmmu dataset.
+    """Testcase: Verify that the inference accuracy of the mistralai/Mistral-Small-3.1-24B-Instruct-2503 model on the MMMU dataset is no less than 0.2.
 
     [Test Category] Model
     [Test Target] mistralai/Mistral-Small-3.1-24B-Instruct-2503
