1447f8a40f6e9cd0f01e76a31a72bbc7da7f235e
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 22:06:46 +0800

    Update test_ascend_chatglm2_6b.py

--- test/registered/ascend/llm_models/test_ascend_chatglm2_6b.py
+++ test/registered/ascend/llm_models/test_ascend_chatglm2_6b.py
@@ -9,7 +9,7 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestChatGlm2(GSM8KAscendMixin, CustomTestCase):
-    """Testcase:Test the accuracy of the ZhipuAI/chatglm2-6b model using the GSM8K dataset.
+    """Testcase: Verify that the inference accuracy of the ZhipuAI/chatglm2-6b model on the GSM8K dataset is no less than 0.25.
 
     [Test Category] Model
     [Test Target] ZhipuAI/chatglm2-6b
