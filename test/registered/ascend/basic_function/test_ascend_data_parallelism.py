e0641100c75736846e87217316e359225bb2d391
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-29 16:42:02 +0800

    Update test_ascend_data_parallelism.py

--- test/registered/ascend/basic_function/test_ascend_data_parallelism.py
+++ test/registered/ascend/basic_function/test_ascend_data_parallelism.py
@@ -7,6 +7,7 @@ import requests
 from sglang.srt.utils import kill_process_tree
 from sglang.test.run_eval import run_eval
 from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.ascend.test_ascend_utils import Llama_3_1_8B_Instruct_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -26,9 +27,7 @@ class TestDataParallelism(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.model = (
-            "/root/.cache/modelscope/hub/models/AI-ModelScope/Llama-3.1-8B-Instruct"
-        )
+        cls.model = Llama_3_1_8B_Instruct_WEIGHTS_PATH
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = (
             [
