c649e1f545a639d4c14a2a005be936bfe600a99b
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 15:59:03 +0800

    Update test_ascend_utils.py

--- python/sglang/test/ascend/test_ascend_utils.py
+++ python/sglang/test/ascend/test_ascend_utils.py
@@ -7,3 +7,4 @@ MODEL_WEIGHTS_DIR = "/root/.cache/modelscope/hub/models/"
 
 # LLM model weights path
 MiniCPM_O_2_6_WEIGHTS_PATH = os.path.join(MODEL_WEIGHTS_DIR, "openbmb/MiniCPM-o-2_6")
+Llama_3_1_8B_Instruct_WEIGHTS_PATH = os.path.join(MODEL_WEIGHTS_DIR, "AI-ModelScope/Llama-3.1-8B-Instruct")
