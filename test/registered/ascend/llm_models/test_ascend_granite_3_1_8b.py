f894fd2dc83c67a76a13c186860b1bf3419ec7ea
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 21:20:00 +0800

    Update test_ascend_granite_3_1_8b.py

--- test/registered/ascend/llm_models/test_ascend_granite_3_1_8b.py
+++ test/registered/ascend/llm_models/test_ascend_granite_3_1_8b.py
@@ -9,7 +9,7 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestGranite(GSM8KAscendMixin, CustomTestCase):
-    """Testcase:Test the accuracy of the ibm-granite/granite-3.1-8b-instruct model using the GSM8K dataset.
+    """Testcase: Verify that the inference accuracy of the ibm-granite/granite-3.1-8b-instruct model on the GSM8K dataset is no less than 0.695.
 
     [Test Category] Model
     [Test Target] ibm-granite/granite-3.1-8b-instruct
