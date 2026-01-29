ef154f51160f7dba948765f99762ea8de3615291
shun8686 <verve@vip.qq.com>
2026-01-29 20:30:04 +0800

    Optimized test case.

--- python/sglang/test/ascend/performance/run_ascend_testcase.sh
+++ python/sglang/test/ascend/performance/run_ascend_testcase.sh
@@ -36,7 +36,7 @@ sysctl -w vm.swappiness=0
 sysctl -w kernel.numa_balancing=0
 sysctl -w kernel.sched_migration_cost_ns=50000
 
-#export PYTHONPATH=$SGLANG_SOURCE_PATH/python:$PYTHONPATH
+export PYTHONPATH=$SGLANG_SOURCE_PATH/python:$PYTHONPATH
 export SGLANG_SET_CPU_AFFINITY=1
 
 unset https_proxy
