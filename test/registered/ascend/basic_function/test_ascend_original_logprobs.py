62aa597870b36dcc9a64ffbd4d24f1ad2bf0b63f
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-29 16:04:03 +0800

    Update test_ascend_original_logprobs.py

--- test/registered/ascend/basic_function/test_ascend_original_logprobs.py
+++ test/registered/ascend/basic_function/test_ascend_original_logprobs.py
@@ -24,12 +24,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 import sglang as sgl
 from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
+from sglang.test.ascend.test_ascend_utils import Llama_3.2_1B_Instruct_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 
 # ------------------------- Configurable via env ------------------------- #
-MODEL_ID = (
-    "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-1B-Instruct"
-)
+MODEL_ID = Llama_3.2_1B_Instruct_WEIGHTS_PATH
+
 PROMPTS = [
     "Hello, my name is",
     "The future of AI is",
