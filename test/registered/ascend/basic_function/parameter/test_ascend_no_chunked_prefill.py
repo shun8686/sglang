1cf97c2da581af95529a364805de91cd010719ff
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-29 19:10:10 +0800

    Update test_ascend_no_chunked_prefill.py

--- test/registered/ascend/basic_function/parameter/test_ascend_no_chunked_prefill.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_no_chunked_prefill.py
@@ -28,7 +28,7 @@ class TestNoChunkedPrefill(CustomTestCase):
 
     def test_no_chunked_prefill_without_radix_cache(self):
         res = run_bench_serving(
-            model=DEFAULT_MODEL_NAME_FOR_TEST,
+            model=Llama_3_1_8B_Instruct_WEIGHTS_PATH,
             num_prompts=10,
             request_rate=float("inf"),
             other_server_args=["--disable-radix-cache", "--chunked-prefill-size", "-1"],
