3802e5e20a591b7adf2f92c9f81c38b3f7484e0d
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 16:26:49 +0800

    Update test_ascend_disable_chunked_prefill.py

--- test/registered/ascend/basic_function/parameter/test_ascend_disable_chunked_prefill.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_disable_chunked_prefill.py
@@ -4,7 +4,6 @@ from types import SimpleNamespace
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.srt.utils import kill_process_tree
 from sglang.test.run_eval import run_eval
-from sglang.test.ascend.test_ascend_utils import Llama_3.1_8B_Instruct_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
