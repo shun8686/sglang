c3051191ffaa814c4351120448dcc11dc9b5feca
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 22:05:21 +0800

    Update test_ascend_afm_4_5b.py

--- test/registered/ascend/llm_models/test_ascend_afm_4_5b.py
+++ test/registered/ascend/llm_models/test_ascend_afm_4_5b.py
@@ -9,7 +9,7 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestAFM(GSM8KAscendMixin, CustomTestCase):
-    """Testcase:Test the accuracy of the arcee-ai/AFM-4.5B-Base model using the GSM8K dataset.
+    """Testcase: Verify that the inference accuracy of the arcee-ai/AFM-4.5B-Base model on the GSM8K dataset is no less than 0.375.
 
     [Test Category] Model
     [Test Target] arcee-ai/AFM-4.5B-Base
