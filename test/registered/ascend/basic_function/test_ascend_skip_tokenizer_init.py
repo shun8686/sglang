13834b2f2bdbb858e570125a335645c0c61ca5a0
YZY00Raiser <2452658175@qq.com>
2026-01-29 19:28:08 +0800

    update test_ascend_skip_tokenizer_init

--- test/registered/ascend/basic_function/test_ascend_skip_tokenizer_init.py
+++ test/registered/ascend/basic_function/test_ascend_skip_tokenizer_init.py
@@ -18,7 +18,9 @@ from sglang.test.test_utils import (
     CustomTestCase,
     popen_launch_server,
 )
+from sglang.test.ci.ci_register import register_npu_ci
 
+register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 class TestSkipTokenizerInit(CustomTestCase):
     """Testcase：Verify set --skip_tokenizer_init parameter, the inference request is successful.
