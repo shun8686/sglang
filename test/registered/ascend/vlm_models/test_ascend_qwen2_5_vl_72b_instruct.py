3cedfdaf06f0253908da573206757aecc79fe900
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 23:30:05 +0800

    Update test_ascend_qwen2_5_vl_72b_instruct.py

--- test/registered/ascend/vlm_models/test_ascend_qwen2_5_vl_72b_instruct.py
+++ test/registered/ascend/vlm_models/test_ascend_qwen2_5_vl_72b_instruct.py
@@ -8,7 +8,7 @@ register_npu_ci(est_time=400, suite="nightly-8-npu-a3", nightly=True)
 
 
 class TestQwen25VL72B(TestVLMModels):
-    """Testcase:Test the accuracy of the Qwen/Qwen2.5-VL-72B-Instruct model using the mmmu dataset.
+    """Testcase: Verify that the inference accuracy of the Qwen/Qwen2.5-VL-72B-Instruct model on the MMMU dataset is no less than 0.3.
 
     [Test Category] Model
     [Test Target] Qwen/Qwen2.5-VL-72B-Instruct
