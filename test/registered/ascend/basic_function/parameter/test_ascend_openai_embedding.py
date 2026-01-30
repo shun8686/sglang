39738b6f6c977d4ac166bdf86f1155b25e3c4da3
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-01-30 19:15:33 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_openai_embedding.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_openai_embedding.py
@@ -5,7 +5,6 @@ import openai
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import gte_Qwen2_1_5B_instruct_WEIGHTS_PATH
 from sglang.test.test_utils import (
-    DEFAULT_SMALL_EMBEDDING_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
@@ -33,11 +32,7 @@ class TestOpenAIEmbedding(CustomTestCase):
         cls.api_key = "sk-123456"
 
         # Configure embedding-specific args
-        other_args = (
-            ["--is-embedding", "--enable-metrics", "--attention-backend", "ascend"]
-            if is_npu()
-            else ["--is-embedding", "--enable-metrics"]
-        )
+        other_args = ["--is-embedding", "--enable-metrics", "--attention-backend", "ascend"]
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
