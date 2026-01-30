c3116f81b5f6808aa9c6f89e4c3e8c25db8474ce
cy <chenyang08056032@163.com>
2026-01-30 11:36:06 +0800

    modify

--- test/registered/ascend/vlm_models/test_ascend_llava_next_72b.py
+++ test/registered/ascend/vlm_models/test_ascend_llava_next_72b.py
@@ -1,33 +1,39 @@
 import unittest
 
-from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
 from sglang.test.ascend.test_ascend_utils import LLAVA_NEXT_72B_WEIGHTS_PATH
+from sglang.test.ascend.vlm_utils import TestVLMModels
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.test_utils import CustomTestCase
 
 register_npu_ci(est_time=400, suite="nightly-16-npu-a3", nightly=True)
 
 
-class TestLlavaNext72B(GSM8KAscendMixin, CustomTestCase):
-    """Testcase:Test the accuracy of the lmms-lab/llava-next-72b model using the GSM8K dataset.
+class TestLlavaNext72B(TestVLMModels):
+    """Testcase: Verify that the inference accuracy of the lmms-lab/llava-next-72b model on the MMMU dataset is no less than 0.2.
 
     [Test Category] Model
     [Test Target] lmms-lab/llava-next-72b
     """
 
     model = LLAVA_NEXT_72B_WEIGHTS_PATH
-    accuracy = 0.79
+    mmmu_accuracy = 0.2
     other_args = [
         "--trust-remote-code",
-        "--tp-size",
-        16,
+        "--cuda-graph-max-bs",
+        "32",
+        "--enable-multimodal",
         "--mem-fraction-static",
-        "0.8",
+        0.35,
+        "--log-level",
+        "info",
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
-        "--enable-multimodal",
+        "--tp-size",
+        16,
     ]
+
+    def test_vlm_mmmu_benchmark(self):
+        self._run_vlm_mmmu_test()
 
 
 if __name__ == "__main__":
