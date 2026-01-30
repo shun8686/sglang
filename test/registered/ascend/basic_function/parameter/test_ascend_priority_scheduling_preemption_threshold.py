55e6d9cf9e26c16b16e68968fee6375e0019c5d1
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-30 11:13:26 +0800

    Update test_ascend_priority_scheduling_preemption_threshold.py

--- test/registered/ascend/basic_function/parameter/test_ascend_priority_scheduling_preemption_threshold.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_priority_scheduling_preemption_threshold.py
@@ -6,7 +6,7 @@ from typing import Any, List, Optional, Tuple
 
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_Instruct_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
