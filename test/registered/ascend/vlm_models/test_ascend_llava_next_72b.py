c04c77690385346520517d8227744006aec6d567
cy <chenyang08056032@163.com>
2026-01-30 12:51:20 +0800

    modify

--- test/registered/ascend/vlm_models/test_ascend_llava_next_72b.py
+++ test/registered/ascend/vlm_models/test_ascend_llava_next_72b.py
@@ -4,7 +4,7 @@ from sglang.test.ascend.test_ascend_utils import LLAVA_NEXT_72B_WEIGHTS_PATH
 from sglang.test.ascend.vlm_utils import TestVLMModels
 from sglang.test.ci.ci_register import register_npu_ci
 
-register_npu_ci(est_time=400, suite="nightly-16-npu-a3", nightly=True)
+register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
 
 
 class TestLlavaNext72B(TestVLMModels):
@@ -18,18 +18,14 @@ class TestLlavaNext72B(TestVLMModels):
     mmmu_accuracy = 0.2
     other_args = [
         "--trust-remote-code",
-        "--cuda-graph-max-bs",
-        "32",
-        "--enable-multimodal",
+        "--tp-size",
+        4,
         "--mem-fraction-static",
-        0.35,
-        "--log-level",
-        "info",
+        "0.8",
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
-        "--tp-size",
-        16,
+        "--enable-multimodal",
     ]
 
     def test_vlm_mmmu_benchmark(self):
