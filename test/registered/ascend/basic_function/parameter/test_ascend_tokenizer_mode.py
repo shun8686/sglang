7af798962dc0a25162f373be4367e674de90eaca
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-02 17:26:33 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_tokenizer_mode.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_tokenizer_mode.py
@@ -1,5 +1,4 @@
 import unittest
-
 import requests
 
 from sglang.srt.utils import kill_process_tree
@@ -23,7 +22,6 @@ class TestEnableTokenizerModeSlow(CustomTestCase):
     [Test Category] Parameter
     [Test Target] --tokenizer-mode slow/auto
     """
-
 
     tokenizer_mode = "slow"
 
@@ -54,52 +52,31 @@ class TestEnableTokenizerModeSlow(CustomTestCase):
         kill_process_tree(cls.process.pid)
 
     def test_tokenzier_mode(self):
-        model_path = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-        self.base_url = DEFAULT_URL_FOR_TEST
-        for tokenizer_mode in ["slow", "auto"]:
-            other_args = [
-                "--tokenizer-mode",
-                tokenizer_mode,
-                "--attention-backend",
-                "ascend",
-                "--disable-cuda-graph",
-                "--tokenizer-path",
-                model_path,
-                "--tokenizer-worker-num",
-                4,
-            ]
+        response = requests.get(f"{self.base_url}/health_generate")
+        self.assertEqual(response.status_code, 200)
 
-            process = popen_launch_server(
-                model_path,
-                self.base_url,
-                timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-                other_args=other_args,
-            )
+        response = requests.post(
+            f"{self.base_url}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
 
-            try:
-                response = requests.get(f"{self.base_url}/health_generate")
-                self.assertEqual(response.status_code, 200)
+        response = requests.get(self.base_url + "/get_server_info")
+        self.assertEqual(response.status_code, 200)
+        print(response.json())
+        self.assertEqual(response.json()["tokenizer_path"], model_path)
+        self.assertEqual(response.json()["tokenizer_mode"], tokenizer_mode)
 
-                response = requests.post(
-                    f"{self.base_url}/generate",
-                    json={
-                        "text": "The capital of France is",
-                        "sampling_params": {
-                            "temperature": 0,
-                            "max_new_tokens": 32,
-                        },
-                    },
-                )
-                self.assertEqual(response.status_code, 200)
-                self.assertIn("Paris", response.text)
 
-                response = requests.get(self.base_url + "/get_server_info")
-                self.assertEqual(response.status_code, 200)
-                print(response.json())
-                self.assertEqual(response.json()["tokenizer_path"], model_path)
-                self.assertEqual(response.json()["tokenizer_mode"], tokenizer_mode)
-            finally:
-                kill_process_tree(process.pid)
+class TestEnableTokenizerModeAuto(TestEnableTokenizerModeSlow):
+    tokenizer_mode = "auto"
 
 
 if __name__ == "__main__":
