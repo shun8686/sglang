d0670e3cec576f7af15b80518f0da354cbc89763
cy <chenyang08056032@163.com>
2026-02-02 14:22:58 +0800

    modify

--- test/registered/ascend/basic_function/parameter/test_ascend_model_loader_extra_config_dsv3_2.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_model_loader_extra_config_dsv3_2.py
@@ -6,7 +6,7 @@ from types import SimpleNamespace
 from urllib.parse import urlparse
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_EXP_W8A8_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_EXP_W8A8_WEIGHTS_PATH, run_command
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_utils import (
@@ -16,18 +16,6 @@ from sglang.test.test_utils import (
 )
 
 register_npu_ci(est_time=400, suite="nightly-16-npu-a3", nightly=True)
-
-
-def run_command(cmd, shell=True):
-    try:
-        result = subprocess.run(
-            cmd, shell=shell, capture_output=True, text=True, check=True
-        )
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        print(f"execute command error: {e}")
-        return None
-
 
 MULTITHREAD_OUT_LOG = "./multi_thread_out_log.txt"
 MULTITHREAD_ERR_LOG = "./multi_thread_err_log.txt"
