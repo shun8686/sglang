ada049095d0b0cbbbacfa9c4ccee4209e24ddabe
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-02 08:28:26 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_graph_tp2_bf16.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_graph_tp2_bf16.py
@@ -1,13 +1,13 @@
 import unittest
 
-from sglang.test.ascend.base_test_ascend_gsm8k_and_throughput import TestAscendGsm8kAndThroughput
+from sglang.test.ascend.base_test_ascend_gsm8k_and_throughput import BaseTestAscendGsm8kAndThroughput
 from sglang.test.ascend.test_ascend_utils import QWEN2_5_7B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=200, suite="nightly-2-npu-a3", nightly=True)
 
 
-class TestAscendGraphTp2Bf16(TestAscendGsm8kAndThroughput):
+class TestAscendGraphTp2Bf16(BaseTestAscendGsm8kAndThroughput):
     """
     Testcase：Verify the accuracy on gsm8k dataset and throughput of Qwen2.5-7B when cuda graph mode is enabled and
     tp size is 2
