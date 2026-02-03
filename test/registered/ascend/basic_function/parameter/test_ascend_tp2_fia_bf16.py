281dc296056cfd33336b3b10b71a6e2291d6908c
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-03 16:48:37 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_tp2_fia_bf16.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_tp2_fia_bf16.py
@@ -7,7 +7,7 @@ register_npu_ci(est_time=250, suite="nightly-2-npu-a3", nightly=True)
 class TestAscendTp2FIABf16(TestAscendGraphTp1Bf16):
     """
     Testcase：Verify the correctness and performance when kernels for attention layers are chosen, cuda graph mode is
-    disabled, tp size is 2 and FIA acceleration is used， and defaults to auto-detection the device to use if not specified
+    disabled, radix cache is disabled, tp size is 2 and FIA acceleration is used.
 
     [Test Category] Parameter
     [Test Target] --attention-backend, --disable-cuda-graph, --tp-size, --disable-radix-cache
