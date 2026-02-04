e0d39755f1310407b0f35fbd04f3c5c7fa2c13c0
YZY00Raiser <2452658175@qq.com>
2026-02-04 19:53:51 +0800

    update test_ascend_debug_tensor_input_file

--- test/registered/ascend/basic_function/parameter/test_ascend_debug_tensor_input_file.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_debug_tensor_input_file.py
@@ -1,7 +1,7 @@
 import os
 import unittest
-
 import numpy
+from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -13,6 +13,7 @@ from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
+
 class TestDebugTensorInputFile(CustomTestCase):
     """Testcase：Verify set --debug-tensor-dump-input-file parameter, the -debug-tensor-dump-input-file is taking effect.
 
@@ -20,39 +21,45 @@ class TestDebugTensorInputFile(CustomTestCase):
        [Test Target] --debug-tensor-dump-input-file
        """
     model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-    def test_tensor_input_file(self):
+
+    @classmethod
+    def setUpClass(cls):
         vector = numpy.array([1001, 1002, 1003, 1004, 1005, 1006, 1007])
         numpy.save("./input_tensor.npy", vector)
-        other_args = (
-            [
-                "--debug-tensor-dump-input-file",
-                "./input_tensor.npy",
-                "--attention-backend",
-                "ascend",
-                "--disable-cuda-graph",
-            ]
-
+        other_args = [
+            "--debug-tensor-dump-input-file",
+            "./input_tensor.npy",
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+        ]
+        cls.out_log_file = open("./tensor_input_out_log.txt", "w+", encoding="utf-8")
+        cls.err_log_file = open("./tensor_input_err_log.txt", "w+", encoding="utf-8")
+        cls.process = popen_launch_server(
+            cls.model,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
-        out_log_file = open("./tensor_input_out_log.txt", "w+", encoding="utf-8")
-        err_log_file = open("./tensor_input_err_log.txt", "w+", encoding="utf-8")
-        try:
-            process = popen_launch_server(
-                self.model,
-                DEFAULT_URL_FOR_TEST,
-                timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-                other_args=other_args,
-                return_stdout_stderr=(out_log_file, err_log_file),
-            )
-        except Exception as e:
-            print("process is killed")
-        err_log_file.seek(0)
-        content = err_log_file.read()
-        self.assertTrue(len(content) > 0)
-        out_log_file.close()
-        err_log_file.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+        cls.out_log_file.close()
+        cls.err_log_file.close()
         os.remove("./tensor_input_out_log.txt")
         os.remove("./tensor_input_err_log.txt")
         os.remove("./input_tensor.npy")
+
+    def test_tensor_input_file(self):
+        print("-----------------out==0------------")
+        print(self.out_log_file)
+        print("-----------------out==0------------")
+        print(self.err_log_file)
+        self.err_log_file.seek(0)
+        content = self.err_log_file.read()
+        self.assertTrue(len(content) > 0)
 
 
 if __name__ == "__main__":
