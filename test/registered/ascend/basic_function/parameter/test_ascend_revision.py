2ade7a707393c55c6ad50c7ecad2d001eda44577
YZY00Raiser <2452658175@qq.com>
2026-02-04 09:38:12 +0800

    update test_ascend_revision

--- test/registered/ascend/basic_function/parameter/test_ascend_revision.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_revision.py
@@ -1,8 +1,6 @@
 import unittest
-
 import requests
 from sglang.test.ascend.test_ascend_utils import PHI_4_MULTIMODAL_INSTRUCT_WEIGHTS_PATH
-
 from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -15,69 +13,15 @@ from sglang.test.ci.ci_register import register_npu_ci
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
-class TestRevisionDefault(CustomTestCase):
+class TestRevision(CustomTestCase):
     """Testcase：Verify set --revision parameter, the inference request is successfully processed.
 
-       [Test Category] Parameter
-       [Test Target] --revision
-       """
-    model = PHI_4_MULTIMODAL_INSTRUCT_WEIGHTS_PATH
-    revision = "33e62acdd07cd7d6635badd529aa0a3467bb9c6a"
-
-    @classmethod
-    def setUpClass(cls):
-        other_args = (
-            [
-                "--attention-backend",
-                "ascend",
-                "--weight-loader-disable-mmap",
-                "--disable-cuda-graph",
-                "--trust-remote-code",
-            ]
-
-        )
-
-        cls.process = popen_launch_server(
-            cls.model,
-            DEFAULT_URL_FOR_TEST,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
-
-    def test_revision_default(self):
-        response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/generate",
-            json={
-                "text": "The capital of France is",
-                "sampling_params": {
-                    "temperature": 0,
-                    "max_new_tokens": 32,
-                },
-            },
-        )
-        print(response.text)
-        self.assertEqual(
-            response.status_code, 200, "The request status code is not 200."
-        )
-        self.assertIn(
-            "Paris", response.text, "The inference result does not include Paris."
-        )
-
-        response = requests.get(f"{DEFAULT_URL_FOR_TEST}/get_server_info")
-        print(response.json())
-        self.assertEqual(
-            response.status_code, 200, "The request status code is not 200."
-        )
-        self.assertIsNone(response.json()["revision"])
-
-
-class TestRevision(CustomTestCase):
+           [Test Category] Parameter
+           [Test Target] --revision
+           """
     revision = "33e62acdd07cd7d6635badd529aa0a3467bb9c6a"
     model = PHI_4_MULTIMODAL_INSTRUCT_WEIGHTS_PATH
+
     @classmethod
     def setUpClass(cls):
         other_args = (
@@ -89,9 +33,7 @@ class TestRevision(CustomTestCase):
                 "--revision",
                 cls.revision,
             ]
-
         )
-
         cls.process = popen_launch_server(
             cls.model,
             DEFAULT_URL_FOR_TEST,
@@ -121,7 +63,6 @@ class TestRevision(CustomTestCase):
         self.assertIn(
             "Paris", response.text, "The inference result does not include Paris."
         )
-
         response = requests.get(f"{DEFAULT_URL_FOR_TEST}/get_server_info")
         print(response.json())
         self.assertEqual(
@@ -132,6 +73,55 @@ class TestRevision(CustomTestCase):
             self.revision,
             "--revision is not taking effect.",
         )
+
+
+class TestRevisionDefault(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        other_args = (
+            [
+                "--attention-backend",
+                "ascend",
+                "--weight-loader-disable-mmap",
+                "--disable-cuda-graph",
+                "--trust-remote-code",
+            ]
+        )
+        cls.process = popen_launch_server(
+            cls.model,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_revision_default(self):
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        print(response.text)
+        self.assertEqual(
+            response.status_code, 200, "The request status code is not 200."
+        )
+        self.assertIn(
+            "Paris", response.text, "The inference result does not include Paris."
+        )
+        response = requests.get(f"{DEFAULT_URL_FOR_TEST}/get_server_info")
+        print(response.json())
+        self.assertEqual(
+            response.status_code, 200, "The request status code is not 200."
+        )
+        self.assertIsNone(response.json()["revision"])
 
 
 if __name__ == "__main__":
