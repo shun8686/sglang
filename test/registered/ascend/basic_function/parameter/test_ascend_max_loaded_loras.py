e7452e3f617b4ed6f6bba3abc99a4f4ac123c448
yangfen0903 <2253770994@qq.com>
2026-02-04 20:15:17 +0800

    change

--- test/registered/ascend/basic_function/parameter/test_ascend_max_loaded_loras.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_max_loaded_loras.py
@@ -1,8 +1,11 @@
+import os
 import unittest
+from types import SimpleNamespace
 
 import requests
 
 from sglang.srt.utils import kill_process_tree
+from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_WEIGHTS_PATH, \
     LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
@@ -40,7 +43,6 @@ class TestMaxLoadedLoras(CustomTestCase):
             f"lora_1={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
             f"lora_2={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
             f"lora_3={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
-            f"lora_4={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
         ]
         cls.process = popen_launch_server(
             LLAMA_3_2_1B_WEIGHTS_PATH,
@@ -76,6 +78,54 @@ class TestMaxLoadedLoras(CustomTestCase):
             response.json()["max_loaded_loras"],
             3,
         )
+
+
+class TestMaxLoadedLorasError(TestMaxLoadedLoras):
+    """Testcase: Test The number of LoRA paths should exceed max_loaded_loras
+
+    [Test Category] --lora-backend
+    [Test Target] --max-loaded-loras
+    """
+    def test_max_loaded_loras_error(self):
+        error_message = ("The number of LoRA paths should not exceed max_loaded_loras.")
+        other_args = [
+            "--enable-lora",
+            "--max-loaded-loras",
+            3,
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--mem-fraction-static",
+            0.8,
+            "--max-loras-per-batch",
+            1,
+            "--lora-path",
+            f"lora_1={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
+            f"lora_2={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
+            f"lora_3={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
+            f"lora_4={LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH}",
+        ]
+        out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+        try:
+            process = popen_launch_server(
+                LLAMA_3_2_1B_WEIGHTS_PATH,
+                DEFAULT_URL_FOR_TEST,
+                timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+                other_args=other_args,
+                return_stdout_stderr=(out_log_file, err_log_file),
+            )
+        except Exception as e:
+            print(f"Server launch failed as expectes:{e}")
+        finally:
+            err_log_file.seek(0)
+            content = err_log_file.read()
+            # error_message information is recorded in the error log
+            self.assertIn(error_message, content)
+            out_log_file.close()
+            err_log_file.close()
+            os.remove("./cache_out_log.txt")
+            os.remove("./cache_err_log.txt")
 
 
 if __name__ == "__main__":
