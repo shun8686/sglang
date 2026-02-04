fd6fcea787733dc91d2d3f8ab4cacde92625add7
Sugar920 <2845757609@qq.com>
2026-02-04 20:34:53 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_debug_tensor_input_file.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_debug_tensor_input_file.py
@@ -2,42 +2,31 @@ import os
 import unittest
 
 import numpy
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
 )
-from sglang.test.ci.ci_register import register_npu_ci
 
-register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 class TestDebugTensorInputFile(CustomTestCase):
-    """Testcase：Verify set --debug-tensor-dump-input-file parameter, the -debug-tensor-dump-input-file is taking effect.
-
-       [Test Category] Parameter
-       [Test Target] --debug-tensor-dump-input-file
-       """
-    model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
     def test_tensor_input_file(self):
         vector = numpy.array([1001, 1002, 1003, 1004, 1005, 1006, 1007])
         numpy.save("./input_tensor.npy", vector)
-        other_args = (
-            [
-                "--debug-tensor-dump-input-file",
-                "./input_tensor.npy",
-                "--attention-backend",
-                "ascend",
-                "--disable-cuda-graph",
-            ]
-
-        )
+        other_args = [
+            "--debug-tensor-dump-input-file",
+            "./input_tensor.npy",
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+        ]
         out_log_file = open("./tensor_input_out_log.txt", "w+", encoding="utf-8")
         err_log_file = open("./tensor_input_err_log.txt", "w+", encoding="utf-8")
         try:
-            process = popen_launch_server(
-                self.model,
+            popen_launch_server(
+                "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-1B-Instruct",
                 DEFAULT_URL_FOR_TEST,
                 timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
                 other_args=other_args,
@@ -48,6 +37,7 @@ class TestDebugTensorInputFile(CustomTestCase):
         err_log_file.seek(0)
         content = err_log_file.read()
         self.assertTrue(len(content) > 0)
+        # self.assertIn("The server is fired up and ready to roll!", content)
         out_log_file.close()
         err_log_file.close()
         os.remove("./tensor_input_out_log.txt")
