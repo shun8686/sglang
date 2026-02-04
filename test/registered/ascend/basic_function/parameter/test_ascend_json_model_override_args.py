e4ddbda2187e3a74fc346bc554cc781346b6e4ed
YZY00Raiser <2452658175@qq.com>
2026-02-04 16:15:08 +0800

    test_ascend_json_model_override_args

--- test/registered/ascend/basic_function/parameter/test_ascend_json_model_override_args.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_json_model_override_args.py
@@ -69,9 +69,9 @@ class TestJsonModelOverrideArgs(CustomTestCase):
             response.status_code, 200, "The request status code is not 200."
         )
         self.assertEqual(
-            response.json()["--json-model-override-args"],
-            '{"max_position_embeddings": 50}',
-            "--moe-runner-backend is not taking effect.",
+            response.json()["max_position_embeddings"],
+            "50",
+            "--json-model-override-args is not taking effect.",
         )
         self.assertEqual(response.status_code, 400)
         self.assertIn("longer than the model's context length", response.text)
