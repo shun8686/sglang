9d3cce3253b1130b26f1c7e65a9bd572fe929778
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-06 15:40:28 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_grpc_mode.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_grpc_mode.py
@@ -1,5 +1,6 @@
 import requests
 import unittest
+import subprocess
 from urllib.parse import urlparse
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import QWEN2_0_5B_INSTRUCT_WEIGHTS_PATH
@@ -26,31 +27,36 @@ class TestAscendGrpcMode(CustomTestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = QWEN2_0_5B_INSTRUCT_WEIGHTS_PATH
-        cls.back_up_model_path = cls.model + "-back-up"
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.url = urlparse(cls.base_url)
-        cls.common_args = [
-            "--trust-remote-code",
-            "--mem-fraction-static", 0.8,
-            "--attention-backend", "ascend",
-            "--grpc-mode", "--port", 20000,
+        cls.grpc_base_url = f"grpc://127.0.0.1:20000"
+        cls.grpc_url = urlparse(cls.grpc_base_url)
+
+        worker_command = [
+            "python3",
+            "-m", "sglang.launch_server",
+            "--model", cls.model,
+            "--grpc-mode", "--port", cls.grpc_url.port,
         ]
+        cls.worker_process = subprocess.Popen(worker_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-
-        cls.process = popen_launch_server(
-            cls.back_up_model_path,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=[
-                *cls.common_args,
-            ],
-        )
+        router_command = [
+            "python3",
+            "-m", "sglang_router.launch_router",
+            "--worker-urls", cls.grpc_base_url,
+            "--model-path", cls.model,
+            "--reasoning-parser", "deepseek-r1",
+            "--tool-call-parser", "json",
+            "--host", "0.0.0.0", "--port", "8080",
+        ]
+        cls.router_process = subprocess.Popen(router_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     @classmethod
     def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
+        kill_process_tree(cls.worker_process.pid)
+        kill_process_tree(cls.router_process.pid)
 
-    def test_delete_ckpt_after_loading(self):
+    def test_delete_grpc_mode(self):
         response = requests.post(
             f"{self.base_url}/generate",
             json={
@@ -62,21 +68,33 @@ class TestAscendGrpcMode(CustomTestCase):
             },
         )
 
-        self.assertEqual(
-            response.status_code, 200, "The request status code is not 200."
-        )
-
-
-        self.assertIn(
-            "Paris", response.text, "The inference result does not include Paris."
-        )
+        self.assertEqual(response.status_code, 200, "The request status code is not 200.")
+        self.assertIn("Paris", response.text, "The inference result does not include Paris.")
 
         response = requests.get(f"{self.base_url}/get_server_info")
         self.assertEqual(
             response.status_code, 200, "The request status code is not 200."
         )
-        print(response.json())
-        print(response.json()["--grpc-mode"])
+        self.assertEqual(
+            response.json()['grpc_mode'], True, "The Grpc mode is not started."
+            "The fastapi root path is not correct."
+        )
+
+        response = requests.post(
+            f"{self.grpc_url}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+
+        self.assertEqual(response.status_code, 200, "The request status code is not 200.")
+        self.assertIn("Paris", response.text, "The inference result does not include Paris.")
+
+
 
 
 if __name__ == "__main__":
