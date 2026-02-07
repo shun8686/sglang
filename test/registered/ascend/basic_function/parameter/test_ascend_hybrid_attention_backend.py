32f78d52c0e305467679c7369b3c5a2a7f22277b
YZY00Raiser <2452658175@qq.com>
2026-02-07 14:09:49 +0800

    update test_ascend_hybrid_attention_backend

--- test/registered/ascend/basic_function/parameter/test_ascend_hybrid_attention_backend.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_hybrid_attention_backend.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 from types import SimpleNamespace
-
 import requests
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.srt.utils import kill_process_tree
