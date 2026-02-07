a31ec56c23835812440cddfd3e7d019f3f809fe5
YZY00Raiser <2452658175@qq.com>
2026-02-07 15:23:48 +0800

    update test_ascend_enable_profile_cuda_graph

--- test/registered/ascend/basic_function/parameter/test_ascend_enable_profile_cuda_graph.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_enable_profile_cuda_graph.py
@@ -1,42 +1,40 @@
 import unittest
-
 import requests
-from sglang.test.ascend.test_ascend_utils import PHI_4_MULTIMODAL_INSTRUCT_WEIGHTS_PATH
 from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
 )
+
 from sglang.test.ci.ci_register import register_npu_ci
 
-register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+register_npu_ci(est_time=50, suite="nightly-1-npu-a3", nightly=True)
 
 
-class TestDisableFastImageProcessor(CustomTestCase):
-    """Testcase：Verify set --disable-fast-image-processor, can normally handle multimodal (picture+text) requests.
+class TestEnableProfileCudaGraph(CustomTestCase):
+    """
+    Testcase：Verify that the --enable-profile-cuda-graph parameter is correctly configured and the inference result is correctly
 
-       [Test Category] Parameter
-       [Test Target] --disable-fast-image-processor
-       """
-    model = PHI_4_MULTIMODAL_INSTRUCT_WEIGHTS_PATH
-    IMAGE_SGL_LOGO_URL = "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg"
+    [Test Category] Parameter
+    [Test Target] --enable-profile-cuda-graph
+    """
 
     @classmethod
     def setUpClass(cls):
-        other_args = (
-            [
-                "--disable-fast-image-processor",
-                "--attention-backend",
-                "ascend",
-                "--disable-cuda-graph",
-                "--trust-remote-code",
-            ]
-        )
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        other_args = [
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--enable-profile-cuda-graph",
+        ]
+
         cls.process = popen_launch_server(
-            cls.model,
-            DEFAULT_URL_FOR_TEST,
+            LLAMA_3_2_1B_WEIGHTS_PATH,
+            cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=other_args,
         )
@@ -45,40 +43,26 @@ class TestDisableFastImageProcessor(CustomTestCase):
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
 
-    def test_disable_fast_image_processor(self):
+    def test_enable_profile_cuda_graph(self):
         response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/v1/chat/completions",
+            f"{self.base_url}/generate",
             json={
-                "model": self.model,
-                "max_tokens": 50,
-                "messages": [
-                    {
-                        "role": "system",
-                        "content": [
-                            {"type": "text", "text": "You are a helpful assistant."}
-                        ],
-                    },
-                    {
-                        "role": "user",
-                        "content": [
-                            {
-                                "type": "image_url",
-                                "image_url": {"url": self.IMAGE_SGL_LOGO_URL},
-                            },
-                            {
-                                "type": "text",
-                                "text": "What is the content of the caption ?",
-                            },
-                        ],
-                    },
-                ],
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
             },
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("caption", response.text)
-        response = requests.get(DEFAULT_URL_FOR_TEST + "/get_server_info")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["disable_fast_image_processor"], True)
+        self.assertEqual(response.status_code, 200, "The request status code is not 200.")
+        self.assertIn("Paris", response.text, "The inference result does not include Paris.")
+
+        response = requests.get(f"{self.base_url}/get_server_info")
+        self.assertEqual( response.status_code, 200, "The request status code is not 200.")
+        self.assertTrue(
+            response.json()["enable_profile_cuda_graph"],
+            "--enable-profile-cuda-graph is not taking effect.",
+        )
 
 
 if __name__ == "__main__":
