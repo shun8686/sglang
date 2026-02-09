f4e6ec1a6618ed0f130e7ac3f7b1c2f1b99a7f1a
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-09 16:13:02 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_grpc_mode.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_grpc_mode.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 from urllib.parse import urlparse
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import QWEN2_0_5B_INSTRUCT_WEIGHTS_PATH
+#from sglang.test.ascend.test_ascend_utils import QWEN2_0_5B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
