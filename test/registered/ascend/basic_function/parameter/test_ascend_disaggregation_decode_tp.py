78d780f4c1c6aa197ca2c011da474e72cb1d9eda
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-02-09 21:33:58 +0800

    Update test_ascend_disaggregation_decode_tp.py

--- test/registered/ascend/basic_function/parameter/test_ascend_disaggregation_decode_tp.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_disaggregation_decode_tp.py
@@ -18,8 +18,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-base_id = int(os.environ.get("ASCEND_RT_VISIBLE_DEVICES", "0")[0])
-BASE_PORT_FOR_ASCEND_MF = 20000 + base_id * 1000 +66
+base_port = int(os.environ.get("ASCEND_RT_VISIBLE_DEVICES", "0")[0])
+BASE_PORT_FOR_ASCEND_MF = 20000 + base_port * 1000 +66
 os.environ["ASCEND_MF_STORE_URL"] = f"tcp://127.0.0.1:{BASE_PORT_FOR_ASCEND_MF}"
 
 register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
