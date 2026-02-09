b10a2606d947111daedf9c5451909e837fc59792
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-02-09 21:20:12 +0800

    Update test_ascend_disaggregation_decode_dp.py

--- test/registered/ascend/basic_function/parameter/test_ascend_disaggregation_decode_dp.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_disaggregation_decode_dp.py
@@ -87,7 +87,7 @@ class TestDisaggregationDecodeDp(TestDisaggregationBase):
                 "--disaggregation-mode",
                 "decode",
                 "--base-gpu-id",
-                "2",
+                "os.environ.get("ASCEND_RT_VISIBLE_DEVICES")[2]",
                 "--disaggregation-transfer-backend",
                 "ascend",
                 "--disable-cuda-graph",
