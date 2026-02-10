d2c2341375f541c9e1c7d8beb8df5f6df19578f5
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-10 18:28:55 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_soft_watchdog.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_soft_watchdog.py
@@ -1,12 +1,11 @@
 import io
 import unittest
-from sys import exception
 
 import requests
 
+from sglang.srt.environ import envs
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ci.ci_register import register_cuda_ci
-from sglang.test.ascend.test_ascend_utils import QWEN3_0_6B_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -17,37 +16,27 @@ from sglang.test.test_utils import (
 register_cuda_ci(est_time=120, suite="nightly-1-gpu", nightly=True)
 
 
-class TestSoftWatchdog(CustomTestCase):
-    """
-    Testcase：Verify that the correctness and stability of --soft-watchdog mechanism under long-running generation
-    requests on GPU backend
-
-    [Test Category] Parameter
-    [Test Target] --soft-watchdog-timeout
-    """
-
+class BaseTestSoftWatchdog:
     env_override = None
-    expected_message = "Watchdog timeout"
+    expected_message = None
 
     @classmethod
     def setUpClass(cls):
-        cls.model_path = QWEN3_0_6B_WEIGHTS_PATH
-        cls.base_url = DEFAULT_URL_FOR_TEST
-
         cls.stdout = io.StringIO()
         cls.stderr = io.StringIO()
 
-        cls.process = popen_launch_server(
-            cls.model_path,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=[
-                "--soft-watchdog-timeout",
-                "20",
-                "--skip-server-warmup",
-            ],
-            return_stdout_stderr=(cls.stdout, cls.stderr),
-        )
+        with cls.env_override():
+            cls.process = popen_launch_server(
+                "Qwen/Qwen3-0.6B",
+                DEFAULT_URL_FOR_TEST,
+                timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+                other_args=[
+                    "--soft-watchdog-timeout",
+                    "20",
+                    "--skip-server-warmup",
+                ],
+                return_stdout_stderr=(cls.stdout, cls.stderr),
+            )
 
     @classmethod
     def tearDownClass(cls):
@@ -56,38 +45,32 @@ class TestSoftWatchdog(CustomTestCase):
         cls.stderr.close()
 
     def test_watchdog_triggers(self):
-        # print("Start call /generate API", flush=True)
-        # try:
-        #     requests.post(
-        #         self.base_url + "/generate",
-        #         json={
-        #             "text": "Hello, please repeat this sentence for 1000 times.",
-        #             "sampling_params": {"max_new_tokens": 100, "temperature": 0},
-        #         },
-        #         timeout=30,
-        #     )
-        # except requests.exceptions.ReadTimeout as e:
-        #     print(f"requests.post timeout (but expected): {e}")
-        # print("End call /generate API", flush=True)
-
-        with self.assertRaises(requests.exceptions.ReadTimeout) as cm:
+        print("Start call /generate API", flush=True)
+        try:
             requests.post(
-                self.base_url + "/generate",
+                DEFAULT_URL_FOR_TEST + "/generate",
                 json={
                     "text": "Hello, please repeat this sentence for 1000 times.",
                     "sampling_params": {"max_new_tokens": 100, "temperature": 0},
                 },
                 timeout=30,
             )
-
-        e = cm.exception
+        except requests.exceptions.ReadTimeout as e:
+            print(f"requests.post timeout (but expected): {e}")
+        print("End call /generate API", flush=True)
 
         combined_output = self.stdout.getvalue() + self.stderr.getvalue()
         self.assertIn(self.expected_message, combined_output)
-        self.assertIn(self.expected_message, str(e))
-        self.assertEqual(e.request.method, "POST")
-        self.assertIn("/generate", e.request.url)
-        self.assertEqual(e.timeout, 1)
+
+
+class TestSoftWatchdogDetokenizer(BaseTestSoftWatchdog, CustomTestCase):
+    env_override = lambda: envs.SGLANG_TEST_STUCK_DETOKENIZER.override(30)
+    expected_message = "DetokenizerManager watchdog timeout"
+
+
+class TestSoftWatchdogTokenizer(BaseTestSoftWatchdog, CustomTestCase):
+    env_override = lambda: envs.SGLANG_TEST_STUCK_TOKENIZER.override(30)
+    expected_message = "TokenizerManager watchdog timeout"
 
 
 if __name__ == "__main__":
