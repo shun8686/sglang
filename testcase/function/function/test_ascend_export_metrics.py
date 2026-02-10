7ff6930c830a9a03e128050ecf1741a87d5b8542
yangfen0903 <2253770994@qq.com>
2026-02-10 22:51:52 +0800

    add

--- testcase/function/function/test_ascend_export_metrics.py
+++ testcase/function/function/test_ascend_export_metrics.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 
 import requests
@@ -15,7 +14,6 @@ from sglang.test.test_utils import (
 class TestDecodeLogInterval(CustomTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.message = "This may cause latency jitter. Consider calling the freeze_gc API after sending a few warmup requests"
         cls.model = "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-1B"
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = [
@@ -23,17 +21,15 @@ class TestDecodeLogInterval(CustomTestCase):
             "ascend",
             "--disable-cuda-graph",
             "--disable-radix-cache",
-            "--gc_warning_threshold_secs",
-            0.00001,
+            "--export-metrics-to-file",
+            "--export-metrics-to-file-dir",
+            "/tmp"
         ]
-        cls.out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
-        cls.err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=other_args,
-            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
 
     @classmethod
@@ -56,14 +52,7 @@ class TestDecodeLogInterval(CustomTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("Paris", response.text)
 
-        self.err_log_file.seek(0)
-        content = self.err_log_file.read()
-        self.assertIn(self.message, content)
-        self.out_log_file.close()
-        self.err_log_file.close()
-        os.remove("./cache_out_log.txt")
-        os.remove("./cache_err_log.txt")
-
 
 if __name__ == "__main__":
+
     unittest.main()
