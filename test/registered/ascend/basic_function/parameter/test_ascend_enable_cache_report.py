e487162c95fdc85615a544a190d2533d61b95218
YZY00Raiser <2452658175@qq.com>
2026-02-11 13:12:11 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_enable_cache_report.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_enable_cache_report.py
@@ -63,8 +63,10 @@ class TestEnableCacheReport(CustomTestCase):
 
                 },
             )
+            print(response.json())
             self.assertEqual(response.status_code, 200)
             if i == 2:
+
                 self.assertIn('"cached_tokens":256', response.text)
 
 
