ad99a377c8a3e38be2097e502dcb09106eb9e28d
YZY00Raiser <2452658175@qq.com>
2026-02-11 13:24:02 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_enable_cache_report.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_enable_cache_report.py
@@ -63,11 +63,10 @@ class TestEnableCacheReport(CustomTestCase):
 
                 },
             )
-            print(response.json())
-
             self.assertEqual(response.status_code, 200)
             if i == 2:
                 cached_tokens = response.json()["usage"]['prompt_tokens_details']['cached_tokens']
+                print(cached_tokens)
                 self.assertEqual(256, cached_tokens)
 
 
