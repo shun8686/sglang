03bb26c70a3ef0b032053b16e6916c766b8ff60e
yangfen0903 <2253770994@qq.com>
2026-02-11 10:27:21 +0800

    add

--- test/registered/ascend/basic_function/parameter/test_ascend_max_loaded_loras.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_max_loaded_loras.py
@@ -1,10 +1,13 @@
 import os
 import unittest
+
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_WEIGHTS_PATH, \
-    LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import (
+    LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH,
+    LLAMA_3_2_1B_WEIGHTS_PATH,
+)
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
