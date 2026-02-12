177bc213dfefe20cdb2ccce69606cc9f7d08de49
shun8686 <verve@vip.qq.com>
2026-02-12 21:08:09 +0800

    Update testcases.

--- test/manual/ascend/temp/run_test.sh
+++ test/manual/ascend/temp/run_test.sh
@@ -3,11 +3,13 @@ pip config set global.trusted-host "pypi.tuna.tsinghua.edu.cn"
 
 pip3 install kubernetes
 
+# copy utils to image if not exist
 sglang_pkg_path=$(pip show sglang | grep Location | awk '{print $2}')
 ascend_test_util_path=${sglang_pkg_path}/sglang/test/ascend/e2e
-
 if [ ! -d "${ascend_test_util_path}" ];then
     cp -r /data/d00662834/dev-0210/sglang/python/sglang/test/ascend/e2e ${ascend_test_util_path}
 fi
+
+export KUBECONFIG=/data/.cache/kb.yaml
 
 python3 ascend_test_suite_e2e_multi_node.py
