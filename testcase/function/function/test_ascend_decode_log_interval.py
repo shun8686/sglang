3f429a151f595e5e577f3857c4f59b512cf7d114
yangfen0903 <2253770994@qq.com>
2026-02-12 11:52:34 +0800

    change

--- testcase/function/function/test_ascend_decode_log_interval.py
+++ testcase/function/function/test_ascend_decode_log_interval.py
@@ -1,4 +1,6 @@
+import math
 import os
+import subprocess
 import unittest
 
 import requests
@@ -11,10 +13,21 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
+def run_command(cmd, shell=True):
+    try:
+        result = subprocess.run(
+            cmd, shell=shell, capture_output=True, text=True, check=True
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"execute command error: {e}")
+        return None
+
 
 class TestDecodeLogInterval(CustomTestCase):
     @classmethod
     def setUpClass(cls):
+        numbers = 10
         cls.model = "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-1B"
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = [
@@ -23,18 +36,17 @@ class TestDecodeLogInterval(CustomTestCase):
             "--disable-cuda-graph",
             "--disable-radix-cache",
             "--decode-log-interval",
-            10,
+            numbers,
         ]
-        out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
-        err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+        cls.out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        cls.err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=other_args,
-            return_stdout_stderr=(out_log_file, err_log_file),
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
-        err_log_file.seek(0)
 
 
     @classmethod
@@ -42,6 +54,7 @@ class TestDecodeLogInterval(CustomTestCase):
         kill_process_tree(cls.process.pid)
 
     def test_decode_log_interval(self):
+        max_tokens = 512
         response = requests.get(f"{DEFAULT_URL_FOR_TEST}/health_generate")
         self.assertEqual(response.status_code, 200)
         response = requests.post(
@@ -50,12 +63,23 @@ class TestDecodeLogInterval(CustomTestCase):
                 "text": "The capital of France is",
                 "sampling_params": {
                     "temperature": 0,
-                    "max_new_tokens": 512,
+                    "max_new_tokens": max_tokens,
                 },
             },
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn("Paris", response.text)
+        result = run_command("cat ./cache_err_log.txt | grep 'Decode batch' | wc -1")
+        response = requests.get(DEFAULT_URL_FOR_TEST + "/get_server_info")
+        self.assertEqual(response.status_code, 200)
+        decode_number = response.json()["--decode-log-interval"]
+        decod_batch_result = math.ceil(max_tokens / decode_number)
+        self.assertIn(decod_batch_result, result)
+        os.remove("./cache_out_log.txt")
+        os.remove("./cache_err_log.txt")
+
+class TestDecodeLogIntervalOther(TestDecodeLogInterval):
+    numbers = 30
 
 
 if __name__ == "__main__":
