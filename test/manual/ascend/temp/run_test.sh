12a6caf458898d3c7cb1139f030e9cca8f61db05
shun8686 <verve@vip.qq.com>
2026-02-12 21:33:28 +0800

    Update testcases.

--- test/manual/ascend/temp/run_test.sh
+++ test/manual/ascend/temp/run_test.sh
@@ -8,6 +8,9 @@ function prepare_env() {
     ascend_test_util_path=${sglang_pkg_path}/sglang/test/ascend/e2e
     rm -rf ${ascend_test_util_path}
     cp -r /data/d00662834/dev-0210/sglang/python/sglang/test/ascend/e2e ${ascend_test_util_path}
+
+    # kubectl
+    cp /data/d00662834/debug/k8s/arm/kubectl /usr/local/sbin/
 }
 
 prepare_env
