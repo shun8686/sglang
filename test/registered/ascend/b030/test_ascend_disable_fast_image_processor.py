972663400badbe8c348fa1b3fb018ea2c6d939f7
cy <chenyang08056032@163.com>
2026-02-12 21:51:43 +0800

    modify

--- test/registered/ascend/b030/test_ascend_disable_fast_image_processor.py
+++ test/registered/ascend/b030/test_ascend_disable_fast_image_processor.py
@@ -14,7 +14,7 @@ class TestQwen3OmniServer(VideoOpenAITestMixin):
         "32",
         "--enable-multimodal",
         "--mem-fraction-static",
-        0.35,
+        0.5,
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
