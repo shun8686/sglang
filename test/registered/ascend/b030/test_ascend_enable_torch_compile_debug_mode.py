2a0f2fff1e6378cb141d95929cdde13724aa1c82
cy <chenyang08056032@163.com>
2026-02-12 14:20:18 +0800

    modify

--- test/registered/ascend/b030/test_ascend_enable_torch_compile_debug_mode.py
+++ test/registered/ascend/b030/test_ascend_enable_torch_compile_debug_mode.py
@@ -23,11 +23,13 @@ class TestEnableTorchCompileDebugMode(CustomTestCase):
         "--disable-cuda-graph",
         "--tp-size",
         "4",
+    ]
+    enable_args = [
         "--enable-torch-compile-debug-mode",
     ]
 
-    def test_gsm8k(self):
-        res = run_bench_serving(
+    def test_enable_torch_compile_debug_mode(self):
+        res1 = run_bench_serving(
             model=self.model,
             dataset_name="random",
             num_prompts=128,
@@ -42,7 +44,32 @@ class TestEnableTorchCompileDebugMode(CustomTestCase):
             gsp_output_len=1,
             other_server_args=self.other_args,
         )
-        print(res)
+        mean_ttft_ms1 = res1["mean_ttft_ms"]
+        mean_tpot_ms1 = res1["mean_tpot_ms"]
+        output_throughput1 = res1["output_throughput"]
+        res2 = run_bench_serving(
+            model=self.model,
+            dataset_name="random",
+            num_prompts=128,
+            random_input_len=3584,
+            random_output_len=1,
+            request_rate=float("inf"),
+            max_concurrency=16,
+            gsp_num_groups=1,
+            gsp_prompts_per_group=128,
+            gsp_system_prompt_len=1792,
+            gsp_question_len=1792,
+            gsp_output_len=1,
+            other_server_args=self.other_args + self.enable_args,
+        )
+        mean_ttft_ms2 = res2["mean_ttft_ms"]
+        mean_tpot_ms2 = res2["mean_tpot_ms"]
+        output_throughput2 = res2["output_throughput"]
+        print(1111111111111111111111111111111111111111111111111)
+        print(mean_ttft_ms1, mean_tpot_ms1, output_throughput1)
+        print(2222222222222222222222222222222222222222222222222)
+        print(mean_ttft_ms2, mean_tpot_ms2, output_throughput2)
+
 
 
 if __name__ == "__main__":
