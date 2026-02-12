29ea4a590406a4152554d90951f4fe147d9c00e1
cy <chenyang08056032@163.com>
2026-02-12 13:15:27 +0800

    modify

--- test/registered/ascend/reward_models/test_ascend_internlm2_7b_reward.py
+++ test/registered/ascend/reward_models/test_ascend_internlm2_7b_reward.py
@@ -50,6 +50,7 @@ class TestInternlm2(CustomTestCase):
             tp_size=4,
             mem_fraction_static=0.8,
         ) as srt_runner:
+            print(os.environ.copy())
             prompts = srt_runner.tokenizer.apply_chat_template(CONVS, tokenize=False)
             srt_outputs = srt_runner.forward(prompts)
         srt_scores = torch.tensor(srt_outputs.scores)
