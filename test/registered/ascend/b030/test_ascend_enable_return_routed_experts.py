899fa897d6b26e6d1a4e90b2e3207c7fb4b9ab3d
cy <chenyang08056032@163.com>
2026-02-12 21:05:16 +0800

    modify

---
+++ test/registered/ascend/b030/test_ascend_enable_return_routed_experts.py
@@ -0,0 +1,68 @@
+import unittest
+
+import requests
+from transformers import AutoTokenizer
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    popen_launch_server, CustomTestCase,
+)
+
+
+class TestEnableReturnRoutedExperts(CustomTestCase):
+    model = DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.9",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        "16",
+        "--quantization",
+        "modelslim",
+        "--disable-radix-cache",
+        "--enable-return-routed-experts"
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=cls.other_args,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_enable_custom_logit_processor(self):
+        response = requests.get(f"{DEFAULT_URL_FOR_TEST}/health_generate")
+        self.assertEqual(response.status_code, 200)
+
+        text1 = "The capital of France is"
+
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": text1,
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+                "return_routed_experts": True,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        print(response)
+
+
+if __name__ == "__main__":
+    unittest.main()
