c0dc401f09d9670933ddeb87bda674882943fb9b
cy <chenyang08056032@163.com>
2026-02-12 21:23:51 +0800

    modify

---
+++ test/registered/ascend/b030/test_ascend_disable_fast_image_processor.py
@@ -0,0 +1,28 @@
+import unittest
+
+from sglang.test.ascend.test_ascend_utils import QWEN3_VL_30B_A3B_INSTRUCT_WEIGHTS_PATH
+from sglang.test.vlm_utils import (
+    OmniOpenAITestMixin,
+)
+
+
+class TestQwen3OmniServer(OmniOpenAITestMixin):
+    model = QWEN3_VL_30B_A3B_INSTRUCT_WEIGHTS_PATH
+    extra_args = [
+        "--trust-remote-code",
+        "--cuda-graph-max-bs",
+        "32",
+        "--enable-multimodal",
+        "--mem-fraction-static",
+        0.35,
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        4,
+        "--disable-fast-image-processor",
+    ]
+
+
+if __name__ == "__main__":
+    unittest.main()
