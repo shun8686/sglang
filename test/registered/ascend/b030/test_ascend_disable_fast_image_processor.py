b75793726100440b17e0969453f8a1a4f6dd6796
cy <chenyang08056032@163.com>
2026-02-13 14:17:26 +0800

    modify

--- test/registered/ascend/b030/test_ascend_disable_fast_image_processor.py
+++ test/registered/ascend/b030/test_ascend_disable_fast_image_processor.py
@@ -1,13 +1,13 @@
 import unittest
 
-from sglang.test.ascend.test_ascend_utils import QWEN3_VL_30B_A3B_INSTRUCT_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import QWEN3_VL_4B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.vlm_utils import (
     OmniOpenAITestMixin, ImageOpenAITestMixin, VideoOpenAITestMixin,
 )
 
 
 class TestQwen3OmniServer(VideoOpenAITestMixin):
-    model = QWEN3_VL_30B_A3B_INSTRUCT_WEIGHTS_PATH
+    model = QWEN3_VL_4B_INSTRUCT_WEIGHTS_PATH
     extra_args = [
         "--trust-remote-code",
         # "--mem-fraction-static",
@@ -15,8 +15,6 @@ class TestQwen3OmniServer(VideoOpenAITestMixin):
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
-        "--tp-size",
-        4,
         "--disable-fast-image-processor",
     ]
 
