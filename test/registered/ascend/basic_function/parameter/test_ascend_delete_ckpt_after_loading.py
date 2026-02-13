2200f2a165e13524197ca6fa54a019bab54adb0f
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-02-13 18:57:14 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_delete_ckpt_after_loading.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_delete_ckpt_after_loading.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import QWEN2_0_5B_INSTRUCT_WEIGHTS_PATH, QWEN3_0_6B_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import QWEN3_0_6B_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -28,11 +28,8 @@ class TestAscendDeleteCkptAfterLoading(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        # cls.model = QWEN2_0_5B_INSTRUCT_WEIGHTS_PATH
         cls.model = QWEN3_0_6B_WEIGHTS_PATH
         cls.back_up_model = cls.model + "-back-up"
-        # cls.source_path = os.path.abspath(repr(cls.model))
-        # cls.target_path = os.path.abspath(repr(cls.back_up_model))
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.url = urlparse(cls.base_url)
         cls.common_args = [
