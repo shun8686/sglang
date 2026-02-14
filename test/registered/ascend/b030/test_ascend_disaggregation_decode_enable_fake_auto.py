944719375b3ede5260e883b3a637abca8c0af87f
cy <chenyang08056032@163.com>
2026-02-14 15:41:08 +0800

    modify

--- test/registered/ascend/b030/test_ascend_disaggregation_decode_enable_fake_auto.py
+++ test/registered/ascend/b030/test_ascend_disaggregation_decode_enable_fake_auto.py
@@ -11,60 +11,45 @@ from sglang.test.server_fixtures.disaggregation_fixture import (
 )
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    popen_launch_server,
+    popen_launch_server, CustomTestCase, DEFAULT_URL_FOR_TEST,
 )
 
 
-class DisaggregationHiCacheBase(PDDisaggregationServerBase):
+class DisaggregationHiCacheBase(CustomTestCase):
     """Base class for disaggregation with HiCache tests"""
+    model = QWEN3_32B_WEIGHTS_PATH
+    decode_args = [
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--trust-remote-code",
+        "--disaggregation-mode",
+        "decode",
+        "--tp-size",
+        "2",
+        "--dp-size",
+        "2",
+        "--base-gpu-id",
+        "0",
+        "--mem-fraction-static",
+        "0.8",
+        "--disaggregation-transfer-backend",
+        "ascend",
+        "--disaggregation-decode-enable-fake-auto",
+        "--load-balance-method",
+        "follow_bootstrap_room",
+    ]
     out_file = open("./out_log.txt", "w+", encoding="utf-8")
     err_file = open("./err_log.txt", "w+", encoding="utf-8")
 
     @classmethod
     def setUpClass(cls):
-        super(DisaggregationHiCacheBase, cls).setUpClass()
-
-        cls.model = QWEN3_32B_WEIGHTS_PATH
-
-        cls.tokenizer = get_tokenizer(cls.model)
-        cls.temp_dir = tempfile.mkdtemp()
-        cls.start_decode()
-
-        # Block until both
-        cls.wait_server_ready(cls.decode_url + "/health")
-
-    @classmethod
-    def start_decode(cls):
-        decode_args = [
-            "--attention-backend",
-            "ascend",
-            "--disable-cuda-graph",
-            "--trust-remote-code",
-            "--disaggregation-mode",
-            "decode",
-            "--tp-size",
-            "2",
-            "--dp-size",
-            "2",
-            "--base-gpu-id",
-            "0",
-            "--mem-fraction-static",
-            "0.8",
-            "--disaggregation-transfer-backend",
-            "ascend",
-            "--disaggregation-decode-enable-fake-auto",
-            "--load-balance-method",
-            "follow_bootstrap_room",
-        ]
-        env = {
-            **os.environ,
-        }
+        cls.decode_url = DEFAULT_URL_FOR_TEST
         cls.process_decode = popen_launch_server(
             cls.model,
             cls.decode_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=decode_args,
-            env=env,
+            other_args=cls.decode_args,
             return_stdout_stderr=(cls.out_file, cls.err_file),
         )
 
