2e3fa2917928e097f842a3e88608777ed04fa63e
Sugar920 <2845757609@qq.com>
2026-02-26 20:32:13 +0800

    add 37，update 13

--- test/registered/ascend/basic_function/parameter/test_ascend_fim_completion.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_fim_completion.py
@@ -4,6 +4,10 @@ import openai
 
 from sglang.srt.utils import kill_process_tree
 from sglang.srt.utils.hf_transformers_utils import get_tokenizer
+from sglang.test.ascend.test_ascend_utils import (
+    DEEPSEEK_CODER_1_3_B_BASE_PATH,
+    DEEPSEEK_CODER_JSON_PATH,
+)
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -22,7 +26,7 @@ class TestFimCompletion(CustomTestCase):
     [Test Target] --completion-template
     """
 
-    model = "/root/.cache/modelscope/hub/models/deepseek-ai/deepseek-coder-1.3b-base"
+    model = DEEPSEEK_CODER_1_3_B_BASE_PATH
     other_args = [
         "--completion-template",
         "deepseek_coder",
@@ -86,7 +90,7 @@ class TestFimCompletion(CustomTestCase):
 class TestFimCompletionJson(TestFimCompletion):
     other_args = [
         "--completion-template",
-        "./deepseek_coder.json",
+        DEEPSEEK_CODER_JSON_PATH,
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
