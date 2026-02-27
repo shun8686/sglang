e2c8f7d46aa7da689643932ce56f8bad70e3ec3c
cy <chenyang08056032@163.com>
2026-02-27 16:46:07 +0800

    modify

--- test/manual/ascend/performance/test_ascend_deepseek_r1_w8a8_1p1d_16p_in3k5_out1k5_50ms.py
+++ test/manual/ascend/performance/test_ascend_deepseek_r1_w8a8_1p1d_16p_in3k5_out1k5_50ms.py
@@ -48,7 +48,7 @@ MODEL_CONFIG = {
         "--tp-size",
         16,
         "--mem-fraction-static",
-        0.7,
+        0.8,
         "--quantization",
         "modelslim",
         "--max-running-requests",
