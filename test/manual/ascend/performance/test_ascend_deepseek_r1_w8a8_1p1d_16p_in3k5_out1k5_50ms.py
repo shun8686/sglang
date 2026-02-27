aa65f0e4967db16b31b0fc137a5c1cab413973bd
cy <chenyang08056032@163.com>
2026-02-27 17:25:38 +0800

    modify

--- test/manual/ascend/performance/test_ascend_deepseek_r1_w8a8_1p1d_16p_in3k5_out1k5_50ms.py
+++ test/manual/ascend/performance/test_ascend_deepseek_r1_w8a8_1p1d_16p_in3k5_out1k5_50ms.py
@@ -91,7 +91,7 @@ MODEL_CONFIG = {
         "--mem-fraction-static",
         0.8,
         "--max-running-requests",
-        384,
+        24,
         "--quantization",
         "modelslim",
         "--moe-a2a-backend",
