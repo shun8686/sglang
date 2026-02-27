2eebd0ba19095953dc46b66b04086c591371c32e
YZY00Raiser <2452658175@qq.com>
2026-02-27 18:20:21 +0800

    update

--- python/sglang/test/ascend/test_ascend_utils.py
+++ python/sglang/test/ascend/test_ascend_utils.py
@@ -239,6 +239,7 @@ QWEN2_5_MATH_RM_72B_WEIGHTS_PATH = os.path.join(
 )
 # Other
 
+
 DEEPSEEK_CODER_JSON_PATH=(
     "/__w/sglang/sglang/test/registered/ascend/basic_function/deepseek_coder.json"
 )
