7187e4a20f09598fb81760632a2b568db7e36e81
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-02 11:03:51 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_ascend_deepep_auto_qwen3_next.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_ascend_deepep_auto_qwen3_next.py
@@ -2,9 +2,9 @@ import os
 import unittest
 from types import SimpleNamespace
 
-# from sglang.test.ascend.test_ascend_utils import QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH
 from sglang.srt.utils import kill_process_tree
-# from sglang.test.ascend.performance.test_ascend_performance_utils import NIC_NAME
+from sglang.test.ascend.performance.test_ascend_performance_utils import NIC_NAME
+from sglang.test.ascend.test_ascend_utils import QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH
 
 from sglang.test.run_eval import run_eval
 from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
@@ -17,21 +17,13 @@ from sglang.test.test_utils import (
 
 from sglang.test.ci.ci_register import register_npu_ci
 
-register_npu_ci(est_time=200, suite="nightly-8-npu-a3", nightly=True)
+register_npu_ci(
+    est_time=200,
+    suite="nightly-8-npu-a3",
+    nightly=True,
+    disabled="run failed，issue link:https://github.com/Ascend/sglang/issues/58",
+)
 
-import psutil
-import socket
-def get_nic_name():
-    """ Return the network interface name corresponding to IP addresses starting with 172. or 192. """
-    for nic, addrs in psutil.net_if_addrs().items():
-        for addr in addrs:
-            if addr.family == socket.AF_INET and (addr.address.startswith("172.") or addr.address.startswith("192.")):
-                print("The nic name matched is {}".format(nic))
-                return nic
-    return None
-
-nic = get_nic_name()
-NIC_NAME = "lo" if nic is None else nic
 
 class TestQwen3Next(CustomTestCase):
     """
@@ -44,9 +36,7 @@ class TestQwen3Next(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        # cls.model = QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH
-        # cls.model = "/root/.cache/modelscope/hub/models/Qwen/Qwen3-Next-80B-A3B-Instruct"
-        cls.model = "/home/weights/Qwen3-Next-80B-A3B-Instruct-WAA8"
+        cls.model = QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.process = popen_launch_server(
             cls.model,
@@ -66,7 +56,6 @@ class TestQwen3Next(CustomTestCase):
                 "--max-total-tokens", 450560,
                 "--moe-a2a-backend", "deepep",
                 "--deepep-mode", "auto",
-                "--quantization", "modelslim",
                 "--chunked-prefill-size", -1,
             ],
             env={
@@ -95,7 +84,6 @@ class TestQwen3Next(CustomTestCase):
             num_examples=8,
             num_threads=32,
         )
-        print("Starting mmlu test...")
         metrics = run_eval(args)
         self.assertGreater(metrics["score"], expect_score)
 
@@ -110,7 +98,6 @@ class TestQwen3Next(CustomTestCase):
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
         )
-        print("Starting gsm8k test...")
         metrics = run_gsm8k(args)
         self.assertGreaterEqual(
             metrics["accuracy"],
