61ca2e14109ed8c06391cb865d8ce40a17a5e2f8
YZY00Raiser <2452658175@qq.com>
2026-03-03 10:18:34 +0800

    update lint

--- test/registered/ascend/basic_function/parameter/test_ascend_moe_runner_backend.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_moe_runner_backend.py
@@ -3,7 +3,9 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import (
+    QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH,
+)
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -42,7 +44,7 @@ class TestMoreRunnerBackendTriton(CustomTestCase):
                 "0.8",
                 "--tp-size",
                 "2",
-            ]
+            ],
         )
 
     @classmethod
