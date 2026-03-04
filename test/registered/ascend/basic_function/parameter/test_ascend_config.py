ea55ece9f5a02f7711222eb1c4ffcbba2aa4f99a
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-04 15:53:58 +0800

    提交测试用例

--- test/registered/ascend/basic_function/parameter/test_ascend_config.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_config.py
@@ -11,7 +11,7 @@ from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
-    popen_launch_server, _create_clean_subprocess_env,
+    popen_launch_server, _create_clean_subprocess_env, _wait_for_server_health,
 )
 
 register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
@@ -58,6 +58,7 @@ class TestAscendConfig(CustomTestCase):
         ]
         cls.process = subprocess.Popen(command, stdout=None, stderr=None,
                                    env=_create_clean_subprocess_env(os.environ.copy()))
+        _wait_for_server_health(cls.process, cls.base_url, None, DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH)
 
     @classmethod
     def tearDownClass(cls):
@@ -79,88 +80,6 @@ class TestAscendConfig(CustomTestCase):
         self.assertIn("Paris", response.text)
 
 
-# class TestConfigPriority(TestConfig):
-#     """Testcase: Verify set the parameter set in the command line have a higher priority than set in config.yaml,
-#     set false model path in in the command, set right model path in in the config.yaml,
-#     will use false model path service start fail .
-#
-#     [Test Category] Parameter
-#     [Test Target] --config
-#     """
-#
-#     model = "/data/Qwen/Qwen3-32B"
-#
-#     @classmethod
-#     def _launch_server(cls):
-#         other_args = cls._build_other_args()
-#         cls.process = popen_launch_server(
-#             cls.model,
-#             DEFAULT_URL_FOR_TEST,
-#             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-#             other_args=other_args,
-#             return_stdout_stderr=(cls.out_log_file, cls.hook_log_file),
-#         )
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.out_log_file_name = "./tmp_out_log.txt"
-#         cls.hook_log_file_name = "./tmp_hook_log.txt"
-#         cls.out_log_file = open(cls.out_log_file_name, "w+", encoding="utf-8")
-#         cls.hook_log_file = open(cls.hook_log_file_name, "w+", encoding="utf-8")
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         kill_process_tree(cls.process.pid)
-#         cls.out_log_file.close()
-#         cls.hook_log_file.close()
-#         os.remove(cls.out_log_file_name)
-#         os.remove(cls.hook_log_file_name)
-#
-#     def test_config(self):
-#         with self.assertRaises(Exception) as ctx:
-#             self._launch_server()
-#         self.assertIn(
-#             "Server process exited with code 1. Check server logs for errors.",
-#             str(ctx.exception),
-#         )
-#         self.hook_log_file.seek(0)
-#         hook_content = self.hook_log_file.read()
-#         self.assertIn(
-#             "make sure '/data/Qwen/Qwen3-32B' is the correct path", hook_content
-#         )
-
-
-# class TestConfigValidation(TestConfig):
-#     """Testcase: Verify set --config exception param the service start fail.
-#
-#     [Test Category] Parameter
-#     [Test Target] --config
-#     """
-#
-#     test_cases = [
-#         "abc",
-#         3.14,
-#         -2,
-#         None,
-#         "!@#$",
-#         "config1.yaml",
-#     ]
-#     for config in test_cases:
-#
-#         @classmethod
-#         def _build_other_args(cls):
-#             return [
-#                 "--config",
-#                 cls.config,
-#             ]
-#
-#         def test_config(self):
-#             with self.assertRaises(Exception) as ctx:
-#                 self._launch_server()
-#             self.assertIn(
-#                 "Server process exited with code 1. Check server logs for errors.",
-#                 str(ctx.exception),
-#             )
 
 
 class TestAscendConfigInValidConfigFileType(CustomTestCase):
