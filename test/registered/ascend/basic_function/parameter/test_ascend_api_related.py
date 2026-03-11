a794106854ab58044349f8c6d5ee69889ef6017f
yangfen0903 <2253770994@qq.com>
2026-03-11 09:20:08 +0800

    提交需求测试用例代码

--- test/registered/ascend/basic_function/parameter/test_ascend_api_related.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_api_related.py
@@ -1,21 +1,20 @@
-import json
 import logging
 import unittest
-import tempfile
-import os
 
 import requests
-
+from types import SimpleNamespace
 from sglang.srt.utils import kill_process_tree
-
+from sglang.test.ascend.test_ascend_utils import (
+    LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
+    run_command,
+)
+from sglang.test.few_shot_gsm8k import run_eval
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
 )
-
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=600, suite="nightly-1-npu-a3", nightly=True)
@@ -30,6 +29,7 @@ class TestApiRelatedApiKey(CustomTestCase):
         cls.api_key = "test-api-key-12345"
         cls.custom_model_name = "Llama3.2"
         cls.weight_version = "v1.0.0"
+        cls.hf_chat_template_name = "tool_use"
         other_args = [
             "--trust-remote-code",
             "--mem-fraction-static",
@@ -43,6 +43,8 @@ class TestApiRelatedApiKey(CustomTestCase):
             cls.custom_model_name,
             "--weight-version",
             cls.weight_version,
+            "--hf-chat-template-name",
+            cls.hf_chat_template_name
         ]
 
         cls.process = popen_launch_server(
@@ -52,18 +54,22 @@ class TestApiRelatedApiKey(CustomTestCase):
             other_args=other_args,
         )
 
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process)
+
     def test_served_model_weight_version(self):
         response = requests.get(f"{self.base_url}/v1/models")
         result = response.json()
 
         self.assertIn("data", result)
+        logging.warning(f"*******{result}")
         self.assertEqual(result["data"][0]["id"], self.custom_model_name)
         self.assertEqual(result["data"][0]["weight_version"], self.weight_version)
-
         logging.warning(f"Request with api-key auth succeeded: {result['text'][:50]}")
         logging.warning(f"Weight version works: {result['text'][:50]}")
 
-    def test_api_key_auth(self):
+    def test_template_name(self):
         headers = {"Authorization": f"Bearer {self.api_key}"}
         response = requests.post(
             f"{self.base_url}/generate",
@@ -79,18 +85,14 @@ class TestApiRelatedApiKey(CustomTestCase):
 
         self.assertIn("text", result)
         self.assertGreater(len(result["text"]), 0)
-        logging.warning(f"Request with api-key auth succeeded: {result['text'][:50]}")
+        logging.warning(f"Request with succeeded: {result['text'][:50]}")
 
-
-class TestApiRelatedAdminApiKey(CustomTestCase):
-
+class TestApiRelatedStoragePath(CustomTestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
         cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.api_key = "test-api-key-12345"
-        cls.custom_model_name = "Llama3.2"
-        cls.weight_version = "v1.0.0"
+        cls.storage_path = "/tmp/storage_path"
         other_args = [
             "--trust-remote-code",
             "--mem-fraction-static",
@@ -98,12 +100,14 @@ class TestApiRelatedAdminApiKey(CustomTestCase):
             "--attention-backend",
             "ascend",
             "--disable-cuda-graph",
-            "--api-key",
-            cls.api_key,
-            "--served-model-name",
-            cls.custom_model_name,
-            "--weight-version",
-            cls.weight_version,
+            "--hicache-storage-backend",
+            "file",
+            "--file-storage-path",
+            cls.storage_path,
+            "--tool-server",
+            "demo",
+            "--tool-call-parser",
+            "llama3",
         ]
 
         cls.process = popen_launch_server(
@@ -113,34 +117,69 @@ class TestApiRelatedAdminApiKey(CustomTestCase):
             other_args=other_args,
         )
 
-    def test_served_model_weight_version(self):
-        response = requests.get(f"{self.base_url}/v1/models")
-        result = response.json()
+    def test_storage_path(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=21000,
+        )
+        run_eval(args)
+        hicache_file = run_command(f"ls {self.storage_path}")
+        self.assertNotEqual(hicache_file, None)
+        hicache_file_size = run_command(f"du -s {self.storage_path} | cut -f1")
+        self.assertGreater(int(hicache_file_size), 0)
+        run_command(f"rm -rf {self.storage_path}")
 
-        self.assertIn("data", result)
-        self.assertEqual(result["data"][0]["id"], self.custom_model_name)
-        self.assertEqual(result["data"][0]["weight_version"], self.weight_version)
+class TestApiRelatedChatTemplate(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.storage_path = "/tmp/storage_path"
+        other_args = [
+            "--trust-remote-code",
+            "--mem-fraction-static",
+            "0.8",
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--sampling-defaults",
+            "openai",
+            "--chat-template",
+            "llama3",
+            "--tool-call-parser",
+            "pythonic",
+        ]
 
-        logging.warning(f"Request with api-key auth succeeded: {result['text'][:50]}")
-        logging.warning(f"Weight version works: {result['text'][:50]}")
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
 
-    def test_api_key_auth(self):
-        headers = {"Authorization": f"Bearer {self.api_key}"}
+    def test_chat_template(self):
         response = requests.post(
-            f"{self.base_url}/generate",
+            f"{self.base_url}/v1/chat/completions",
             json={
-                "text": "Explain neural networks:",
-                "sampling_params": {
-                    "max_new_tokens": 64,
-                },
+                "model": "llama-3.2",
+                "messages": [
+                    {"role": "user", "content": "Hello, how are you?"},
+                ]
             },
-            headers = headers,
         )
         result = response.json()
 
-        self.assertIn("text", result)
-        self.assertGreater(len(result["text"]), 0)
-        logging.warning(f"Request with api-key auth succeeded: {result['text'][:50]}")
+        self.assertIn("choices", result)
+        self.assertGreater(len(result["choices"]), 0)
+        logging.warning(f"Builtin chat template works: {result['choices'][0]['messages'][0]['content'][:50]}...")
+
+if __name__ == "__main__":
+    unittest.main()
 
 
 
