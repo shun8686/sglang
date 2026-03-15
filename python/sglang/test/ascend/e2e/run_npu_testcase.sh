6db96b9bb985f6870a32a973aee8fbcdf0456787
shun8686 <verve@vip.qq.com>
2026-03-15 17:04:18 +0800

    add temp step

--- python/sglang/test/ascend/e2e/run_npu_testcase.sh
+++ python/sglang/test/ascend/e2e/run_npu_testcase.sh
@@ -13,6 +13,9 @@ npu-smi info
 cp -r /root/.cache/.cache/kubernetes /tmp/
 pip3 install --no-index --find-links=/tmp/kubernetes/ kubernetes
 
+# =============temp step====================
+bash /root/sglang/python/sglang/test/ascend/e2e/temp.sh
+
 # copy or download required file
 cp /root/.cache/huggingface/hub/datasets--anon8231489123--ShareGPT_Vicuna_unfiltered/snapshots/192ab2185289094fc556ec8ce5ce1e8e587154ca/ShareGPT_V3_unfiltered_cleaned_split.json /tmp
 #curl -o /tmp/test.jsonl -L https://gh-proxy.test.osinfra.cn/https://raw.githubusercontent.com/openai/grade-school-math/master/grade_school_math/data/test.jsonl
