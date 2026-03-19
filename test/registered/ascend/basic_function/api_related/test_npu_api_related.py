501182300ec01b5ee1fbc0d314461a5e18af24ef
yangfen0903 <2253770994@qq.com>
2026-03-19 20:23:01 +0800

    Submit code

--- test/registered/ascend/basic_function/api_related/test_npu_api_related.py
+++ test/registered/ascend/basic_function/api_related/test_npu_api_related.py
@@ -7,8 +7,8 @@ import requests
 
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import (
-    LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
     C4AI_COMMAND_R_V01_WEIGHTS_PATH,
+    LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
 )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval
