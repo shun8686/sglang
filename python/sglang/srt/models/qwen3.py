74031cfd1a708812a988a6f70f6572e2b5ca7b32
EdwardXuy <yifeng.xu_edward@foxmail.com>
2026-03-20 09:59:27 +0800

    model debug

--- python/sglang/srt/models/qwen3.py
+++ python/sglang/srt/models/qwen3.py
@@ -555,7 +555,8 @@ class Qwen3ForCausalLM(nn.Module):
 
     def set_embed_and_head(self, embed, head):
         del self.model.embed_tokens.weight
-        del self.lm_head.weight
+        if hasattr(self.lm_head,"weight"):
+            del self.lm_head.weight
         self.model.embed_tokens.weight = embed
         self.lm_head.weight = head
         torch.cuda.empty_cache()
