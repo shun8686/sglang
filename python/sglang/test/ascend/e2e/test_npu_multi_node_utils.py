7948c7bee96c618d8992e4bf0496d8916c60b2a5
shun8686 <verve@vip.qq.com>
2026-03-23 15:40:47 +0800

    Bugfix for other_args.

--- python/sglang/test/ascend/e2e/test_npu_multi_node_utils.py
+++ python/sglang/test/ascend/e2e/test_npu_multi_node_utils.py
@@ -528,7 +528,7 @@ def launch_pd_separation_node(model_config):
     host_ip = get_host_ip()
     logger.info(f"Starting {role} node on {host_ip} with args: {service_args}")
 
-    common_args = [
+    other_args = [
         "--trust-remote-code",
         "--attention-backend",
         "ascend",
@@ -540,18 +540,14 @@ def launch_pd_separation_node(model_config):
         "llm",
     ]
 
-    for arg in common_args:
-        if arg not in service_args:
-            service_args.insert(0, arg)
+    other_args.extend(service_args)
 
     try:
         process = popen_launch_server(
             model_config["model_path"],
             f"http://{host_ip}:{PREFILL_DECODE_PORT}",
             timeout=LOCAL_TIMEOUT,
-            other_args=[
-                *service_args,
-            ],
+            other_args=other_args,
         )
     except Exception as e:
         raise RuntimeError(f"Failed to start {role} node on {host_ip}: {e}")
---
+++ test/manual/ascend/performance/v26/test_ascend_deepseek_v3_2_w8a8_1p1d_32p_in128k_out1k_bs16.py
@@ -0,0 +1,156 @@
+import unittest
+
+from sglang.test.ascend.e2e.test_npu_multi_node_utils import NIC_NAME
+from sglang.test.ascend.e2e.test_npu_performance_utils import (
+    DEEPSEEK_V32_W8A8_MODEL_PATH,
+    ROUND_ROBIN,
+    TestAscendPerfMultiNodePdSepTestCaseBase,
+)
+
+MODEL_CONFIG = {
+    "model_path": DEEPSEEK_V32_W8A8_MODEL_PATH,
+    "prefill_envs": {
+        "SGLANG_SET_CPU_AFFINITY": "1",
+        "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+        "STREAMS_PER_DEVICE": "32",
+        "HCCL_BUFFSIZE": "1024",
+        "DEEPEP_NORMAL_LONG_SEQ_ROUND": "5",
+        "DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS": "512",
+        # "SGLANG_NPU_USE_MLAPO": "1",
+        "DEEP_NORMAL_MODE_USE_INT8_QUANT": "1",
+        # "SGLANG_NPU_USE_MULTI_STREAM": "1",
+        "HCCL_OP_EXPANSION_MODE": "AIV",
+        "HCCL_SOCKET_IFNAME": NIC_NAME,
+        "GLOO_SOCKET_IFNAME": NIC_NAME,
+    },
+    "decode_envs": {
+        "SGLANG_SET_CPU_AFFINITY": "1",
+        "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+        "STREAMS_PER_DEVICE": "32",
+        # "SGLANG_NPU_USE_MULTI_STREAM": "1",
+        # "SGLANG_NPU_USE_MLAPO": "1",
+        "HCCL_OP_EXPANSION_MODE": "AIV",
+        "SGLANG_SCHEDULER_SKIP_ALL_GATHER": "1",
+        "TASK_QUEUE_ENABLE": "0",
+        "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+        "SGLANG_ENABLE_SPEC_V2": "1",
+        "HCCL_SOCKET_IFNAME": NIC_NAME,
+        "GLOO_SOCKET_IFNAME": NIC_NAME,
+        "HCCL_BUFFSIZE": "400",
+        "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "16",
+    },
+    "router_envs": {
+        "SGLANG_DP_ROUND_ROBIN": "1",
+    },
+    "prefill_args": [
+        "--nnodes",
+        2,
+        "--tp",
+        32,
+        "--watchdog-timeout",
+        9000,
+        "--mem-fraction-static",
+        0.73,
+        "--disable-radix-cache",
+        "--chunked-prefill-size",
+        65536,
+        "--max-prefill-tokens",
+        65536,
+        "--max-running-requests",
+        16,
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "normal",
+        "--quantization",
+        "modelslim",
+        "--disaggregation-transfer-backend",
+        "ascend",
+        "--disaggregation-mode",
+        "prefill",
+        "--disable-cuda-graph",
+        "--enable-nsa-prefill-context-parallel",
+        "--moe-dense-tp-size",
+        1,
+        "--nsa-prefill-cp-mode",
+        "in-seq-split",
+        "--speculative-algorithm",
+        "NEXTN",
+        "--speculative-num-steps",
+        1,
+        "--speculative-eagle-topk",
+        1,
+        "--speculative-num-draft-tokens",
+        2,
+    ],
+    "decode_args": [
+        "--nnodes",
+        2,
+        "--tp",
+        32,
+        "--dp",
+        8,
+        "--ep",
+        32,
+        "--moe-dense-tp-size",
+        1,
+        "--enable-dp-attention",
+        "--enable-dp-lm-head",
+        "--watchdog-timeout",
+        9000,
+        "--mem-fraction-static",
+        0.79,
+        "--disable-radix-cache",
+        "--chunked-prefill-size",
+        -1,
+        "--max-prefill-tokens",
+        65536,
+        "--max-running-requests",
+        16,
+        "--cuda-graph-max-bs",
+        16,
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "low_latency",
+        "--quantization",
+        "modelslim",
+        "--speculative-algorithm",
+        "NEXTN",
+        "--speculative-num-steps",
+        3,
+        "--speculative-eagle-topk",
+        1,
+        "--speculative-num-draft-tokens",
+        4,
+        "--disaggregation-transfer-backend",
+        "ascend",
+        "--disaggregation-mode",
+        "decode",
+        "--prefill-round-robin-balance",
+        "--load-balance-method",
+        ROUND_ROBIN,
+    ],
+    "router_args": [
+        "--mini-lb",
+    ],
+}
+
+
+class TestDeepSeekV32(TestAscendPerfMultiNodePdSepTestCaseBase):
+    model_config = MODEL_CONFIG
+    dataset_name = "random"
+    max_concurrency = 8
+    num_prompts = 8
+    input_len = 131072
+    output_len = 1024
+    random_range_ratio = 1
+    tpot = 26
+    output_token_throughput = 132.48
+
+    def test_throughput(self):
+        self.run_throughput()
+
+
+if __name__ == "__main__":
+    unittest.main()
