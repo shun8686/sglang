04418166488355a3bbebc4f5e635c4f6dcb7bb67
Sugar920 <2845757609@qq.com>
2026-03-23 21:02:42 +0800

    update

--- test/registered/ascend/basic_function/parameter/runtime_options/test_npu_watchdog.py
+++ test/registered/ascend/basic_function/parameter/runtime_options/test_npu_watchdog.py
@@ -19,27 +19,23 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestBaseTestWatchdog(CustomTestCase):
-    env_override = None
-    expected_crash_message = None
 
     @classmethod
     def setUpClass(cls):
         cls.stdout = io.StringIO()
         cls.stderr = io.StringIO()
 
-        # Simulate blocking of specified module, start service and set watchdog
-        # with cls.env_override():
-        cls.extra_envs = {
-            "SGLANG_TEST_STUCK_DETOKENIZER": "0",
-        }
-        os.environ.update(cls.extra_envs)
+        # cls.extra_envs = {
+        #     "SGLANG_TEST_STUCK_DETOKENIZER": "0",
+        # }
+        # os.environ.update(cls.extra_envs)
         cls.process = popen_launch_server(
             QWEN3_0_6B_WEIGHTS_PATH,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=[
                 "--watchdog-timeout",
-                20,
+                1,
                 "--skip-server-warmup",
                 "--attention-backend",
                 "ascend",
@@ -55,37 +51,35 @@ class TestBaseTestWatchdog(CustomTestCase):
 
     def test_watchdog_crashes_server(self):
         # Verify that the service crashes after watchdog timeout is triggered
+        requests.post(
+            DEFAULT_URL_FOR_TEST + "/generate",
+            json={
+                "text": "Hello, please repeat this sentence for 1000 times.",
+                "sampling_params": {"max_new_tokens": 100, "temperature": 0},
+            },
+            timeout=30,
+        )
 
-        print("Start call /generate API", flush=True)
-        try:
-            response = requests.post(
-                DEFAULT_URL_FOR_TEST + "/generate",
-                json={
-                    "text": "Hello, please repeat this sentence for 1000 times.",
-                    "sampling_params": {"max_new_tokens": 100, "temperature": 0},
-                },
-                timeout=30,
-            )
-            print("=============================")
-            print(response.text)
-        except requests.exceptions.ConnectionError:
-            # Expected connection failure, indicating the service has crashed
-            print("API request failed (expected): Server is crashed as watchdog triggered")
+        # Logs contain service crash keywords
+        combined_output = self.stdout.getvalue() + self.stderr.getvalue()
+        expected_timeout_message = "Scheduler watchdog timeout (self.watchdog_timeout=1.0, self.soft=False)"
+        expected_crash_message = "SIGQUIT received."
+        self.assertIn(
+            expected_timeout_message,
+            combined_output,
+            f"Expected timeout message '{expected_timeout_message}' not found in logs"
+        )
+        self.assertIn(
+            expected_crash_message,
+            combined_output,
+            f"Expected crash message '{expected_crash_message}' not found in logs"
+        )
 
-            # Logs contain service crash keywords
-            combined_output = self.stdout.getvalue() + self.stderr.getvalue()
-            self.assertIn(
-                self.expected_crash_message,
-                combined_output,
-                f"Expected crash message '{self.expected_crash_message}' not found in logs"
-            )
-            print(f"Verified: Found crash message '{self.expected_crash_message}' in logs")
-
-            # # Verify the process has exited (watchdog triggered crash)
-            # self.assertIsNotNone(
-            #     self.process.poll(),
-            #     f"Process should exit after watchdog timeout ({self.watchdog_timeout}s), but still running"
-            # )
+        # Verify the process has exited (watchdog triggered crash)
+        self.assertIsNotNone(
+            self.process.poll(),
+            f"Process should exit after watchdog timeout, but still running"
+        )
 
 
 # class TestWatchdogDetokenizer(BaseTestWatchdog, CustomTestCase):
