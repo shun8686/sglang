255892d4f851f449be8f945a3af5ba315f602a79
cherryblo <136634645@qq.com>
2026-03-23 15:21:39 +0800

    changes

--- test/registered/ascend/basic_function/quant/test_npu_autoround_moe.py
+++ test/registered/ascend/basic_function/quant/test_npu_autoround_moe.py
@@ -4,7 +4,9 @@ from types import SimpleNamespace
 from urllib.parse import urlparse
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import QWEN3_30B_A3B_INSTRUCT_2507_INT4_AUTOROUND_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import (
+QWEN3_30B_A3B_INSTRUCT_2507_INT4_AUTOROUND_WEIGHTS_PATH
+)
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_utils import (
