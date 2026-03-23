16dc816c45d6fed2e1b69a927ffe4adc81f2e30f
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-23 14:54:24 +0800

    test_ascend_load_balance_method.py debug

--- test/registered/ascend/basic_function/parameter/test_pd_disaggretion_decode.sh
+++ test/registered/ascend/basic_function/parameter/test_pd_disaggretion_decode.sh
@@ -1,49 +1,49 @@
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 sysctl -w vm.swappiness=0r
-sysctl -w kernel.numa balancing=0ec
+sysctl -w kernel.numa_balancing=0ec
 sysctl -w kernel.sched_migration_cost_ns=50000
 # bind cpu
-export SGLANG SET CPU AFFINITY=1 
-unset https proxy
-unset http proxy
-unset HTTPS PROXY
-unset HTTP PROXY
-export ASCEND LAUNCH BLOCKING=1 
+export SGLANG_SET_CPU_AFFINITY=1
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+export ASCEND_LAUNCH_BLOCKING=1
 # cann
-source /usr/local/Ascend/ascend-toolkit/set env.sh
-source /usr/local/Ascend/nnal/atb/set env.sh
-export SGLANG DISAGGREGATION WAITING_TIMEOUT=3600
-export STREAMS PER DEVICE=32
-export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=64
-export HCCL BUFFSIZE=2500
-export HCCL_OP_EXPANSION MODE=AIV
-export HCCL SOCKET IFNAME=enp196sOf0
-export GL0O SOCKET IFNAME=enp196s0f0
-export SGLANG NPU PROFILING-0
-#export SGLANG NPU PROFILING STAGE
-export DEEPEP NORMAL_LONG SEQ ROUND=32
-export DEEPEP NORMAL LONG SEQ PER ROUND TOKENS-4096
-export ASCEND_MF_STORE_URL="tcp:/761.47.19.75:24669"
-export SGLANG DISAGGREGATION BOOTSTRAP TIMEOUT=3600
+source /usr/local/Ascend/ascend-toolkit/set/env.sh
+source /usr/local/Ascend/nnal/atb/set/env.sh
 export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
-export SGLANG ENABLE SPEC V2=1
+export STREAMS_PER_DEVICE=32
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=64
+export HCCL_BUFFSIZE=2500
+export HCCL_OP_EXPANSION_MODE=AIV
+export HCCL_SOCKET_IFNAME=enp196sOf0
+export GL0O_SOCKET_IFNAME=enp196s0f0
+export SGLANG_NPU_PROFILING=0
+#export SGLANG_NPU_PROFILING_STAGE
+export DEEPEP_NORMAL_LONG_SEQ_ROUND=32
+export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS-4096
+export ASCEND_MF_STORE_URL="tcp:/127.0.0.1:24669"
+export SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT=3600
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export SGLANG_ENABLE_SPEC_V2=1
 export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
-python3 -m sglang.launch servera
---model-path /home/weights/Qwen3.5-397B-A17B-w8a8
---attention-backend ascend
+python3 -m sglang.launch_server \
+--model-path /home/weights/Qwen3.5-397B-A17B-w8a8 \
+--attention-backend ascend \
 --device npu \
 --tp-size 16 --nnodes 1 --node rank 0 \
-_-chunked-prefill-size -1 --max-prefill-tokens 65536 \
---disable radix-cache
---trust-remote-code l
+--chunked-prefill-size -1 --max-prefill-tokens 65536 \
+--disable radix-cache \
+--trust-remote-code \
 --host 0.0.0.0 --max-running requests 256 \
---moe-a2a-backend deepep
---deepep-mode low latency
---mem-fraction-static 0.85
+--moe-a2a-backend deepep \
+--deepep-mode low_latency \
+--mem-fraction-static 0.85 \
 --port 8000 \
 --cuda-graph-bs 1 2 3 4 8 9 10 11 12 13 14 15 16 \
 --quantization modelslim \
---enable-multimodal
+--enable-multimodal \
 --mm-attention-backend ascend attn --max-total-tokens 1200000 \
---dtype bfloat16 --mamba-ssm-dtype bfloat16 --disaggregation-mode decode --disaggregation-transfer-backend ascend
+--dtype bfloat16 --mamba-ssm-dtype bfloat16 --disaggregation-mode decode --disaggregation-transfer-backend ascend \
 --speculative-draft-model-quantization unquant --speculative-algorithm NEXTN --speculative-num-steps 3 --speculative-eagle-topk 1 --speculative-num-draft-tokens 4
--- test/registered/ascend/basic_function/parameter/test_pd_disaggretion_prefill.sh
+++ test/registered/ascend/basic_function/parameter/test_pd_disaggretion_prefill.sh
@@ -1,40 +1,42 @@
-echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling governor
-sysctl -w vm.swappiness=0
-sysctl -w kernel.numa balancing=0e
-sysctl -w kernel.sched migration cost ns=50000
-# bind cou
-export SGLANG SET CPU AFFINITY=1
-unset https proxy
-unset http proxy
-unset HTTPS PROXY
-unset HTTP PROXY
-export ASCEND LAUNCH BLOCKING=1
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0r
+sysctl -w kernel.numa_balancing=0ec
+sysctl -w kernel.sched_migration_cost_ns=50000
+# bind cpu
+export SGLANG_SET_CPU_AFFINITY=1
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+export ASCEND_LAUNCH_BLOCKING=1
 # cann
-source /usr/local/Ascend/ascend-toolkit/set env.sh
-source /usr/local/Ascend/nnal/atb/set env.sh
-export SGLANG DISAGGREGATION WAITING TIMEOUT-3600
-export STREAMS PER DEVICE=32
-#export SGLANG DEEPEP NUM MAX DISPATCH TOKENS PER RANK=128
-export HCCL BUFFSIZE=1200
-export HCCL OP EXPANSION MODE=AIV
+source /usr/local/Ascend/ascend-toolkit/set/env.sh
+source /usr/local/Ascend/nnal/atb/set/env.sh
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export STREAMS_PER_DEVICE=32
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=64
+export HCCL_BUFFSIZE=2500
+export HCCL_OP_EXPANSION_MODE=AIV
 export HCCL_SOCKET_IFNAME=enp196sOf0
-export GL0O SOCKET IFNAME=enp196sOf0 
-export SGLANG NPU PROFILING=0的
-#export SGLANG NPU PROFILING STAGE="prefill"
-#export DEEPEP NORMAL LONG SEQ ROUND=32
-#export DEEPEP NORMAL LONG SEQ PER ROUND TOKENS=3584
-export ASCEND MF STORE URL="tcp://61.47.19.76:24669"
-export SGLANG DISAGGREGATION BOOTSTRAP TIMEOUT=3600
-0098-LN03WIL ONILIVM NOILV93YO9VSIG ONVTOS 1JOdxa
+export GL0O_SOCKET_IFNAME=enp196s0f0
+export SGLANG_NPU_PROFILING=0
+#export SGLANG_NPU_PROFILING_STAGE
+export DEEPEP_NORMAL_LONG_SEQ_ROUND=32
+export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS-4096
+export ASCEND_MF_STORE_URL="tcp:/127.0.0.1:24669"
+export SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT=3600
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export SGLANG_ENABLE_SPEC_V2=1
+export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
 python3 -m sglang.launch server \
---model-path /home/weights/Qwen3.5-397B-A17B-w8a8
---attention-backend ascendr
+--model-path /home/weights/Qwen3.5-397B-A17B-w8a8 \
+--attention-backend ascendr \
 --device npu \
 --tp-size 16 --nnodes 1 --node-rank 0 \
 --chunked-prefill-size -1 --max-prefill-tokens 16384 \
---disable radix-cache
+--disable radix-cache \
 --trust-remote-code \
---host 0.0.0.0 --max-running requests 64 \
+--host 0.0.0.0 --max-running-requests 64 \
 --mem-fraction-static 0.85 \
 --port 8000 \
 --cuda-graph-bs 2 4 6 8 10 16 20 24 28 32 \
