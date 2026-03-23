d982b16227500e729103ec5a6482ac235fe03ec6
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-23 15:41:01 +0800

    test_ascend_load_balance_method.py debug

--- test/registered/ascend/basic_function/parameter/test_pd_disaggretion_decode.sh
+++ test/registered/ascend/basic_function/parameter/test_pd_disaggretion_decode.sh
@@ -2,9 +2,9 @@ export ASCEND_MF_STORE_URL="tcp://127.0.0.1:8000"
 
 
 python -m sglang.launch_server \
-    --model-path /home/weights/Qwen3-VL-30B-A3B-Instruct \
+    --model-path /root/.cache/modelscope/hub/models/Qwen/Qwen3-30B-A3B-Instruct-2507 \
     --disaggregation-mode decode \
-    --host 141.61.29.204 \
+    --host 127.0.0.1 \
     --port 8081 \
     --trust-remote-code \
     --tp-size 2 \
--- test/registered/ascend/basic_function/parameter/test_pd_disaggretion_prefill.sh
+++ test/registered/ascend/basic_function/parameter/test_pd_disaggretion_prefill.sh
@@ -1,9 +1,10 @@
-export ASCEND_MF_STORE_URL="tcp://141.61.29.204:8000"
+export ASCEND_MF_STORE_URL="tcp://127.0.0.1:8000"
+
 
 python -m sglang.launch_server \
-    --model-path /home/weights/Qwen3-VL-30B-A3B-Instruct \
+    --model-path /root/.cache/modelscope/hub/models/Qwen/Qwen3-30B-A3B-Instruct-2507 \
     --disaggregation-mode prefill \
-    --host 141.61.29.204 \
+    --host 127.0.0.1 \
     --port 8080 \
     --trust-remote-code \
     --base-gpu-id 14 \
@@ -16,4 +17,4 @@ python -m sglang.launch_server \
     --log-level debug \
     --log-level-http debug \
     --disaggregation-bootstrap-port 8998 \
-    --dtype bfloat16 
+    --dtype bfloat16
