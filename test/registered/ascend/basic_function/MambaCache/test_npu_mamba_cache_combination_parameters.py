8bfde2fc17c0b213cf946c9974ddbbe589e9031c
YZY00Raiser <2452658175@qq.com>
2026-03-31 10:21:55 +0800

    update

--- test/registered/ascend/basic_function/MambaCache/test_npu_mamba_cache_combination_parameters.py
+++ test/registered/ascend/basic_function/MambaCache/test_npu_mamba_cache_combination_parameters.py
@@ -1,3 +1,4 @@
+import tempfile
 import unittest
 
 import requests
@@ -41,12 +42,12 @@ class TestMambaCache(CustomTestCase):
             kill_process_tree(cls.process.pid)
 
     def _launch_server_with_mamba_params(
-        self,
-        max_mamba_cache_size=None,
-        mamba_ssm_dtype=None,
-        mamba_full_memory_ratio=0.9,
-        mamba_scheduler_strategy="auto",
-        mamba_track_interval=256,
+            self,
+            max_mamba_cache_size=None,
+            mamba_ssm_dtype=None,
+            mamba_full_memory_ratio=0.9,
+            mamba_scheduler_strategy="auto",
+            mamba_track_interval=256,
     ):
         other_args = [
             "--trust-remote-code",
@@ -211,6 +212,45 @@ class TestMambaCache(CustomTestCase):
             self._test_basic_inference()
         finally:
             kill_process_tree(self.process.pid)
+
+    def test_mamba_track_interval_error(self):
+        # mamba_track_interval not divisible by page_size(128)
+        error_message = "No module named 'cuda'"
+        with tempfile.NamedTemporaryFile(
+                mode="w+", delete=True, suffix="out.log"
+        ) as out_log_file, tempfile.NamedTemporaryFile(
+            mode="w+", delete=True, suffix="out.log"
+        ) as err_log_file:
+            try:
+                popen_launch_server(
+                    self.model,
+                    DEFAULT_URL_FOR_TEST,
+                    timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+                    other_args=[
+                        "--trust-remote-code",
+                        "--mem-fraction-static",
+                        "0.5",
+                        "--attention-backend",
+                        "ascend",
+                        "--disable-cuda-graph",
+                        "--mamba-track-interval",
+                        "127",
+                        "--tp-size",
+                        "8",
+                        "--disable-radix-cache",
+                    ],
+                    return_stdout_stderr=(out_log_file, err_log_file),
+                )
+            except Exception as e:
+                self.assertIn(
+                    "Server process exited with code -9.",
+                    str(e),
+                )
+            finally:
+                err_log_file.seek(0)
+                content = err_log_file.read()
+                # error_message information is recorded in the error log
+                self.assertIn(error_message, content)
 
 
 if __name__ == "__main__":
