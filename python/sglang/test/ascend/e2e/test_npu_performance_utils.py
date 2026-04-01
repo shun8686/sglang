1ff581e1b17bb3a6f3c7893df18d85f68c25bed1
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-01 11:39:15 +0800

    qwen3-5-27b performance debug

--- python/sglang/test/ascend/e2e/test_npu_performance_utils.py
+++ python/sglang/test/ascend/e2e/test_npu_performance_utils.py
@@ -52,7 +52,7 @@ QWEN3_5_27B_MODEL_PATH = (
     "/root/.cache/modelscope/hub/models/Qwen/Qwen3.5-27B"
 )
 QWEN3_5_27B_W8A8_MODEL_PATH = (
-    "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-27B-w8a8-mtp"
+    "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-27B-W8A8"
 )
 QWEN3_30B_A3B_MODEL_PATH = (
     "/root/.cache/modelscope/hub/models/Qwen/Qwen3-30B-A3B-Instruct-2507"
