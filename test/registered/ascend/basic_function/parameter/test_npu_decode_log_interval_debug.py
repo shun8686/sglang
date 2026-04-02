72db356c43821d1636954934182c3d5e04207c60
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-02 18:57:33 +0800

    debug

--- test/registered/ascend/basic_function/parameter/test_npu_decode_log_interval_debug.py
+++ test/registered/ascend/basic_function/parameter/test_npu_decode_log_interval_debug.py
@@ -11,9 +11,7 @@ import openai
 
 from sglang.srt.utils import kill_process_tree
 from sglang.srt.utils.hf_transformers_utils import get_tokenizer
-from sglang.test.ascend.output_capturer import OutputCapturer
-from sglang.test.ascend.test_npu_logging import TestNPULoggingBase
-from sglang.test.ci.ci_register import register_amd_ci, register_cuda_ci, register_npu_ci
+from sglang.test.ci.ci_register import register_amd_ci, register_cuda_ci
 from sglang.test.test_utils import (
     DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -26,53 +24,68 @@ from sglang.test.test_utils import (
 
 register_cuda_ci(est_time=41, suite="stage-b-test-1-gpu-large")
 register_amd_ci(est_time=41, suite="stage-b-test-1-gpu-small-amd")
-register_npu_ci(est_time=100, suite="nightly-1-npu-a3", nightly=True)
 
 
-class TestNPUEnableRequestTimeStatsLogging(TestNPULoggingBase):
-    """Testcase: Verify the functionality of --enable-request-time-stats-logging to generate Req Time Stats logs on Ascend backend with Llama-3.2-1B-Instruct model.
-
-    [Test Category] Parameter
-    [Test Target] --enable-request-time-stats-logging
-    """
-
+class TestLargeMaxNewTokens(CustomTestCase):
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
-        cls.output_capturer = OutputCapturer()
-        cls.output_capturer.start()
-        cls.other_args.extend(["--decode-log-interval",
-                "2",])
+        cls.model = DEFAULT_SMALL_MODEL_NAME_FOR_TEST
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.api_key = "sk-123456"
+
+        cls.stdout = open(STDOUT_FILENAME, "w")
+        cls.stderr = open(STDERR_FILENAME, "w")
+
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=cls.other_args,
+            api_key=cls.api_key,
+            other_args=[
+                "--max-total-token",
+                "1536",
+                "--context-len",
+                "8192",
+                "--decode-log-interval",
+                "2",
+            ],
             env={"SGLANG_CLIP_MAX_NEW_TOKENS_ESTIMATION": "256", **os.environ},
-            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
+            return_stdout_stderr=(cls.stdout, cls.stderr),
         )
+        cls.base_url += "/v1"
+        cls.tokenizer = get_tokenizer(DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
 
     @classmethod
     def tearDownClass(cls):
-        super().tearDownClass()
-        cls.output_capturer.stop()
+        kill_process_tree(cls.process.pid)
+        cls.stdout.close()
+        cls.stderr.close()
+        os.remove(STDOUT_FILENAME)
+        os.remove(STDERR_FILENAME)
 
-    def test_enable_request_time_stats_logging(self):
-        self.inference_once()
-
-        self.assertIn(
-            "Req Time Stats",
-            self.output_capturer.get_all(),
-            f"Keyword not found in server logs.",
+    def run_chat_completion(self):
+        client = openai.Client(api_key=self.api_key, base_url=self.base_url)
+        response = client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": "You are a helpful AI assistant"},
+                {
+                    "role": "user",
+                    "content": "Please repeat the world 'hello' for 10000 times.",
+                },
+            ],
+            temperature=0,
         )
+        return response
 
+    def test_chat_completion(self):
         num_requests = 4
 
         futures = []
         with ThreadPoolExecutor(num_requests) as executor:
             # Send multiple requests
             for i in range(num_requests):
-                futures.append(executor.submit(self.inference_once()))
+                futures.append(executor.submit(self.run_chat_completion))
 
             # Ensure that they are running concurrently
             pt = 0
@@ -88,8 +101,6 @@ class TestNPUEnableRequestTimeStatsLogging(TestNPULoggingBase):
                     pt += 1
 
         assert all_requests_running
-
-
 
 
 if __name__ == "__main__":
