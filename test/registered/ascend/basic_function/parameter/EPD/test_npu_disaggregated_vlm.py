1f23775ba97cd674c88638abe0706ba93d9c3e86
EdwardXuy <yifeng.xu_edward@foxmail.com>
2026-04-03 11:43:57 +0800

    debug

--- test/registered/ascend/basic_function/parameter/EPD/test_npu_disaggregated_vlm.py
+++ test/registered/ascend/basic_function/parameter/EPD/test_npu_disaggregated_vlm.py
@@ -42,7 +42,7 @@ _INLINE_IMAGE_URL = (
 
 class TestDisaggregatedVLM(TestDisaggregationBase):
     __test__ = False
-    transfer_backend: str = None
+    encoder_transfer_backend: str = None: str = None
     """
     Testcase 5.1 & 5.2 Unified: Verify encoder-only + language-only configuration.
 
@@ -110,7 +110,7 @@ class TestDisaggregatedVLM(TestDisaggregationBase):
         encoder_args = [
             "--encoder-only",
             "--encoder-transfer-backend",
-            cls.transfer_backend,
+            cls.encoder_transfer_backend,
             "--tp-size",
             "2",
             "--base-gpu-id",
@@ -147,7 +147,7 @@ class TestDisaggregatedVLM(TestDisaggregationBase):
             "--encoder-urls",
             cls.prefill_url,
             "--encoder-transfer-backend",
-            cls.transfer_backend,
+            cls.encoder_transfer_backend,
             "--tp-size",
             "2",
             "--base-gpu-id",
@@ -297,10 +297,10 @@ class TestDisaggregatedVLM(TestDisaggregationBase):
         super().tearDownClass()
 
 class TestDisaggregatedVLM_ZMQ_Scheduler(TestDisaggregatedVLM):
-    transfer_backend = "zmq_to_scheduler"
+    encoder_transfer_backend = "zmq_to_scheduler"
 
 class TestDisaggregatedVLM_ZMQ_Tokenizer(TestDisaggregatedVLM):
-    transfer_backend = "zmq_to_tokenizer"
+    encoder_transfer_backend = "zmq_to_tokenizer"
 
 if __name__ == "__main__":
     unittest.main()
