2276bc704cccb6ab1014526292021bffdeef2b19
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-08 10:20:21 +0800

    debug

--- python/sglang/test/ascend/test_npu_logging.py
+++ python/sglang/test/ascend/test_npu_logging.py
@@ -66,7 +66,8 @@ class TestNPULoggingBase(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+        # cls.model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+        cls.model = "/home/weights/Llama-3.2-1B-Instruct"
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.other_args = [
             "--trust-remote-code",
@@ -76,6 +77,8 @@ class TestNPULoggingBase(CustomTestCase):
             "ascend",
             "--disable-cuda-graph",
             "--log-requests",
+            "--base-gpu-id",
+            4,
         ]
         cls.out_log_file_obj = tempfile.NamedTemporaryFile(
             mode="w+", encoding="utf-8", delete=False, suffix=".txt"
