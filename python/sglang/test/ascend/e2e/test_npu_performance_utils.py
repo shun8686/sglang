dbb817e410fe474f5e8382fad8d3725d547d8287
shun8686 <verve@vip.qq.com>
2026-04-10 21:30:59 +0800

    Update npu perf testcases.

--- python/sglang/test/ascend/e2e/test_npu_performance_utils.py
+++ python/sglang/test/ascend/e2e/test_npu_performance_utils.py
@@ -109,7 +109,9 @@ KIMI_K2_5_W4A8_MODEL_PATH = "/root/.cache/modelscope/hub/models/Eco-Tech/Kimi-K2
 KIMI_K2_5_EAGLE3_MODEL_PATH = "/root/.cache/modelscope/hub/models/Kimi/kimi-k2.5-eagle3"
 GLM_4_7_FLASH_MODEL_PATH = "/root/.cache/modelscope/hub/models/ZhipuAI/GLM-4.7-Flash"
 
-QWEN3_5_397B_W4A8_MODEL_PATH = "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w4a8-mtp"
+QWEN3_5_397B_W4A8_MODEL_PATH = (
+    "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w4a8-mtp"
+)
 
 ROUND_ROBIN = "round_robin"
 
@@ -380,9 +382,9 @@ def run_aisbench(
         os.path.basename(model_path),
         model_path,
         dataset_path,
-        output_len,
-        max_concurrency,
-        num_prompts,
+        str(output_len),
+        str(max_concurrency),
+        str(num_prompts),
     ]
     process = subprocess.Popen(
         cmd_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1
