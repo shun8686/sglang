76133ea893c0c662eb4d88234809d46774ec151e
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-17 15:41:04 +0800

    Revert "Undo debugging code"
    This reverts commit 8c81f796f0ed7436be71a6d4614b186b0bd3ffc2.

--- test/registered/ascend/performance/v26/req/test_ascend_qwen3_30b_w8a8_4p_in1k_out300_bs16.py
+++ test/registered/ascend/performance/v26/req/test_ascend_qwen3_30b_w8a8_4p_in1k_out300_bs16.py
@@ -1,8 +1,8 @@
 import unittest
 
 from sglang.test.ascend.e2e.test_npu_performance_utils import (
-    QWEN3_30B_A3B_W8A8_MODEL_PATH,
-    QWEN3_A3B_EAGLE_MODEL_PATH,
+    # QWEN3_30B_A3B_W8A8_MODEL_PATH,
+    # QWEN3_A3B_EAGLE_MODEL_PATH,
     TestAscendPerformanceTestCaseBase,
 )
 from sglang.test.ci.ci_register import register_npu_ci
@@ -46,7 +46,8 @@ QWEN3_30B_OTHER_ARGS = [
     "--speculative-algorithm",
     "EAGLE3",
     "--speculative-draft-model-path",
-    QWEN3_A3B_EAGLE_MODEL_PATH,
+    # QWEN3_A3B_EAGLE_MODEL_PATH,
+    "/home/weights/Qwen/Qwen3-a3B_eagle3",
     "--speculative-num-steps",
     5,
     "--speculative-eagle-topk",
@@ -68,12 +69,15 @@ QWEN3_30B_OTHER_ARGS = [
     4,
     "--dtype",
     "bfloat16",
+    "--base-gpu-id",
+    8,
 ]
 
 
 class TestQwen30B(TestAscendPerformanceTestCaseBase):
     max_attempts = 5
-    model = QWEN3_30B_A3B_W8A8_MODEL_PATH
+    # model = QWEN3_30B_A3B_W8A8_MODEL_PATH
+    model = "/home/weights/Qwen/Qwen3-30B-A3B-W8A8"
     other_args = QWEN3_30B_OTHER_ARGS
     envs = QWEN3_30B_ENVS
     dataset_name = "random"
