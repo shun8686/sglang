315a247d95ac0c4afc52948d6da7bcfe36ecf85e
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-17 18:11:51 +0800

    Modify test cases to be consistent with documentation.

--- test/registered/ascend/performance/v26/req/test_ascend_qwen3_30b_w8a8_1p_in1k_out300_bs16.py
+++ test/registered/ascend/performance/v26/req/test_ascend_qwen3_30b_w8a8_1p_in1k_out300_bs16.py
@@ -81,13 +81,13 @@ class TestQwen30B(TestAscendPerformanceTestCaseBase):
     other_args = QWEN3_30B_OTHER_ARGS
     envs = QWEN3_30B_ENVS
     dataset_name = "random"
-    max_concurrency = 16
-    num_prompts = 16
+    max_concurrency = 8
+    num_prompts = 8
     input_len = 1024
     output_len = 300
     random_range_ratio = 1
-    tpot = 7
-    output_token_throughput = 700
+    tpot = 7.47
+    output_token_throughput = 680
 
     def test_qwen3_30b(self):
         self.run_throughput()
