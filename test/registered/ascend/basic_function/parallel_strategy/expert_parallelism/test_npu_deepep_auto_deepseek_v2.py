83685c7c733022f041c9853c6faa7089f597896e
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-23 20:40:58 +0800

    add num_shot=5 in mmlu test for deepseek v2

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_auto_deepseek_v2.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_auto_deepseek_v2.py
@@ -3,10 +3,9 @@ import unittest
 from types import SimpleNamespace
 
 from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.run_eval import run_eval
 from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
-from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -46,10 +45,11 @@ class TestDeepEpDeepseek(CustomTestCase):
                 0.7,
             ],
             env={
-                "SGLANG_ENABLE_JIT_DEEPGEMM": "0",
+                "SGLANG_SET_CPU_AFFINITY": "1",
+                "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+                "STREAMS_PER_DEVICE": "32",
                 "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "512",
-                "HCCL_BUFFSIZE": "2048",
-                "MOE_ENABLE_TOPK_NEG_ONE": "1",
+                "HCCL_BUFFSIZE": "4096",
                 **os.environ,
             },
         )
@@ -66,6 +66,8 @@ class TestDeepEpDeepseek(CustomTestCase):
             eval_name="mmlu",
             num_examples=128,
             num_threads=32,
+            num_shots=5,
+            api="completion",
         )
         print("Starting mmlu test...")
         metrics = run_eval(args)
@@ -74,22 +76,24 @@ class TestDeepEpDeepseek(CustomTestCase):
     def test_gsm8k(self):
         expect_accuracy = 0.34
         args = SimpleNamespace(
-            num_shots=8,
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="gsm8k",
             data_path=None,
-            num_questions=200,
+            num_examples=200,
+            num_threads=128,
+            num_shots=5,
             max_new_tokens=512,
-            parallel=128,
-            host="http://127.0.0.1",
-            port=int(self.base_url.split(":")[-1]),
+            api="completion"
         )
         print("Starting gsm8k test...")
-        metrics = run_gsm8k(args)
+        metrics = run_eval(args)
+        # Assertion: The GSM8K accuracy is not lower than the preset threshold (0.96)
         self.assertGreaterEqual(
-            metrics["accuracy"],
+            metrics["score"],
             expect_accuracy,
-            f'Accuracy of {self.model} is {str(metrics["accuracy"])}, is lower than {expect_accuracy}',
+            f'Accuracy of {self.model} is {str(metrics["score"])}, is lower than {expect_accuracy}',
         )
-
 
 if __name__ == "__main__":
     unittest.main()
