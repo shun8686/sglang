5221abee1bec1180f460b0153dd16868260b64a1
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-25 15:12:26 +0800

    debug

---
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_auto_deepseek_v2_run.py
@@ -0,0 +1,72 @@
+import os
+import unittest
+from time import sleep
+from types import SimpleNamespace
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.e2e.test_npu_accuracy_utils import TestAscendAccuracyTestCaseBase, BENCHMARK_TOOL_DEFAULT
+# from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+register_npu_ci(est_time=400, suite="full-8-npu-a3", nightly=True)
+
+
+ENVS = {
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "STREAMS_PER_DEVICE": "32",
+    "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "512",
+    "HCCL_BUFFSIZE": "4096",
+}
+
+OTHER_ARGS = [
+    "--trust-remote-code",
+    "--attention-backend",
+    "ascend",
+    "--quantization",
+    "modelslim",
+    "--tp-size",
+    "8",
+    "--moe-a2a-backend",
+    "deepep",
+    "--deepep-mode",
+    "auto",
+    "--max-running-requests",
+    128,
+    "--disable-cuda-graph",
+    "--dp-size",
+    8,
+    "--enable-dp-attention",
+    "--chunked-prefill-size",
+    1024,
+    "--mem-fraction-static",
+    0.68,
+    "--base-gpu-id",
+    8,
+    "--log-requests",
+]
+
+class TestDeepEpDeepseek(TestAscendAccuracyTestCaseBase):
+    benchmark_tool = BENCHMARK_TOOL_DEFAULT
+    # model = QWEN3_30B_A3B_W8A8_MODEL_PATH
+    model = "/home/weights/DeepSeek-V2-Lite-W8A8"
+    other_args = ENVS
+    envs = OTHER_ARGS
+    accuracy = 0.1
+    dataset_name = "demo_gsm8k_gen_4_shot_cot_chat_prompt"
+    def test_accuracy(self):
+        sleep(3600)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
