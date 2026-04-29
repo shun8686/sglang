c4b904a6988ab3c89ecffbe19907f3cc4f95d2eb
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-29 21:41:12 +0800

    lint

--- test/registered/ascend/accuracy/test_npu_qwen3_5_27b_1p_in3k5_out1k5_high_aime2025.py
+++ test/registered/ascend/accuracy/test_npu_qwen3_5_27b_1p_in3k5_out1k5_high_aime2025.py
@@ -2,8 +2,10 @@ import unittest
 
 from sglang.test.ascend.e2e.test_npu_accuracy_utils import (
     BENCHMARK_TOOL_DEFAULT,
-    QWEN3_5_27B_W8A8_MODEL_PATH,
     TestAscendAccuracyTestCaseBase,
+)
+from sglang.test.ascend.e2e.test_npu_performance_utils import (
+    QWEN3_5_27B_W8A8_MODEL_PATH,
 )
 from sglang.test.ci.ci_register import register_npu_ci
 
