18b6c9a2e9574a5db85b293896bf33a70324268e
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-05-06 09:44:49 +0800

    Modify deepep-related test cases

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_low_latency_deepseek_R1_0528_w4a8_per_channel.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_low_latency_deepseek_R1_0528_w4a8_per_channel.py
@@ -1,144 +1,96 @@
-import os
 import unittest
-from types import SimpleNamespace
 
-from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.e2e.test_npu_accuracy_utils import TestAscendAccuracyTestCaseBase
 from sglang.test.ascend.test_ascend_utils import (
     DEEPSEEK_R1_0528_W4A8_PER_CHANNEL_WEIGHTS_PATH,
 )
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.run_eval import run_eval
-from sglang.test.test_utils import (
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    DEFAULT_URL_FOR_TEST,
-    CustomTestCase,
-    popen_launch_server,
-)
 
 register_npu_ci(est_time=600, suite="full-16-npu-a3", nightly=True)
 
+ENVS = {
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "STREAMS_PER_DEVICE": "32",
+    "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "256",
+    "HCCL_BUFFSIZE": "3000",
+    "DEEP_NORMAL_MODE_USE_INT8_QUANT": "0",
+    "SGLANG_NPU_USE_MLAPO": "1",
+    "SGLANG_ENABLE_SPEC_V2": "1",
+    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+}
 
-class TestDeepepLowlatencyDeepseekR1(CustomTestCase):
-    """Testcase: Verify the accuracy of DeepSeek-R1 model on MMLU and GSM8K tasks with --deepep-mode low_latency on Ascend NPU backend.
+OTHER_ARGS = [
+    "--tp",
+    "16",
+    "--trust-remote-code",
+    "--attention-backend",
+    "ascend",
+    "--device",
+    "npu",
+    "--quantization",
+    "modelslim",
+    "--watchdog-timeout",
+    "9000",
+    "--cuda-graph-bs",
+    "8",
+    "16",
+    "24",
+    "28",
+    "32",
+    "36",
+    "--mem-fraction-static",
+    "0.6",
+    "--max-running-requests",
+    "144",
+    "--context-length",
+    "8188",
+    "--disable-radix-cache",
+    "--chunked-prefill-size",
+    "512",
+    "--max-prefill-tokens",
+    "4096",
+    "--moe-a2a-backend",
+    "deepep",
+    "--deepep-mode",
+    "low_latency",
+    "--enable-dp-attention",
+    "--dp-size",
+    "4",
+    "--enable-dp-lm-head",
+    "--speculative-algorithm",
+    "NEXTN",
+    "--speculative-num-steps",
+    "3",
+    "--speculative-eagle-topk",
+    "1",
+    "--speculative-num-draft-tokens",
+    "4",
+    "--dtype",
+    "bfloat16",
+]
 
-    [Test Category] Parameter
-    [Test Target] --speculative-algorithm; --deepep-mode
-    """
 
-    accuracy = 0.96
+class TestDeepEpDeepseek(TestAscendAccuracyTestCaseBase):
+    model = DEEPSEEK_R1_0528_W4A8_PER_CHANNEL_WEIGHTS_PATH
+    envs = ENVS
+    other_args = OTHER_ARGS
 
-    @classmethod
-    def setUpClass(cls):
-        cls.model = DEEPSEEK_R1_0528_W4A8_PER_CHANNEL_WEIGHTS_PATH
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.process = popen_launch_server(
-            cls.model,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=[
-                "--tp",
-                "16",
-                "--trust-remote-code",
-                "--attention-backend",
-                "ascend",
-                "--device",
-                "npu",
-                "--quantization",
-                "modelslim",
-                "--watchdog-timeout",
-                "9000",
-                "--cuda-graph-bs",
-                "8",
-                "16",
-                "24",
-                "28",
-                "32",
-                "36",
-                "--mem-fraction-static",
-                "0.6",
-                "--max-running-requests",
-                "144",
-                "--context-length",
-                "8188",
-                "--disable-radix-cache",
-                "--chunked-prefill-size",
-                "512",
-                "--max-prefill-tokens",
-                "4096",
-                "--moe-a2a-backend",
-                "deepep",
-                "--deepep-mode",
-                "low_latency",
-                "--enable-dp-attention",
-                "--dp-size",
-                "4",
-                "--enable-dp-lm-head",
-                "--speculative-algorithm",
-                "NEXTN",
-                "--speculative-num-steps",
-                "3",
-                "--speculative-eagle-topk",
-                "1",
-                "--speculative-num-draft-tokens",
-                "4",
-                "--dtype",
-                "bfloat16",
-            ],
-            env={
-                "SGLANG_SET_CPU_AFFINITY": "1",
-                "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
-                "STREAMS_PER_DEVICE": "32",
-                "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "256",
-                "HCCL_BUFFSIZE": "3000",
-                "DEEP_NORMAL_MODE_USE_INT8_QUANT": "0",
-                "SGLANG_NPU_USE_MLAPO": "1",
-                "SGLANG_ENABLE_SPEC_V2": "1",
-                "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
-                **os.environ,
-            },
-        )
+    def test_gsm8k_accuracy(self):
+        self.dataset_type = "gsm8k"
+        self.dataset_name = "demo_gsm8k_gen_4_shot_cot_chat_prompt"
+        self.accuracy = 0.96
+        self.num_prompts = 200
+        self.run_accuracy()
 
-    @classmethod
-    def tearDownClass(cls):
-        # Terminate the model server process and its child processes after all tests in the class complete
-        kill_process_tree(cls.process.pid)
-
-    def test_mmlu(self):
-        # Test Scenario: Verify the model's general knowledge performance on the MMLU dataset
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="mmlu",
-            num_examples=8,
-            num_threads=32,
-        )
-
-        # Execute MMLU evaluation and retrieve performance metrics
-        metrics = run_eval(args)
-        # Assertion: Ensure the MMLU score meets the basic performance threshold
-        self.assertGreater(metrics["score"], 0.5)
-
-    def test_gsm8k(self):
-        # Test Scenario: Verify the model's accuracy on GSM8K dataset (mathematical reasoning evaluation)
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="gsm8k",
-            data_path=None,
-            num_examples=200,
-            num_threads=300,
-            num_shots=5,
-            max_tokens=512,
-        )
-        # Execute GSM8K evaluation and get metrics
-        metrics = run_eval(args)
-        # Assertion: The GSM8K accuracy is not lower than the preset threshold (0.96)
-        self.assertGreaterEqual(
-            metrics["score"],
-            self.accuracy,
-            f'Accuracy of {self.model} is {str(metrics["score"])}, is lower than {self.accuracy}',
-        )
+    def test_mmlu_accuracy(self):
+        self.dataset_type = "mmlu"
+        self.dataset_name = "mmlu_gen_5_shot_str"
+        self.accuracy = 0.86
+        self.num_prompts = 200
+        self.run_accuracy()
 
 
 if __name__ == "__main__":
     unittest.main()
+
--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_low_latency_deepseek_v2.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_low_latency_deepseek_v2.py
@@ -1,94 +1,67 @@
-import os
 import unittest
-from types import SimpleNamespace
 
-from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.e2e.test_npu_accuracy_utils import TestAscendAccuracyTestCaseBase
 from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
-from sglang.test.run_eval import run_eval
-from sglang.test.test_utils import (
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    DEFAULT_URL_FOR_TEST,
-    CustomTestCase,
-    popen_launch_server,
-)
 
 register_npu_ci(est_time=400, suite="full-8-npu-a3", nightly=True)
 
+ENVS = {
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "STREAMS_PER_DEVICE": "32",
+    "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "512",
+    "HCCL_BUFFSIZE": "4096",
+}
 
-class TestDeepEpDeepseek(CustomTestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.model = DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.process = popen_launch_server(
-            cls.model,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=[
-                "--trust-remote-code",
-                "--attention-backend",
-                "ascend",
-                "--tp-size",
-                "8",
-                "--moe-a2a-backend",
-                "deepep",
-                "--deepep-mode",
-                "low_latency",
-                "--disable-cuda-graph",
-                "--dp-size",
-                8,
-                "--enable-dp-attention",
-                "--chunked-prefill-size",
-                1024,
-                "--mem-fraction-static",
-                0.7,
-            ],
-            env={
-                "SGLANG_ENABLE_JIT_DEEPGEMM": "0",
-                "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "512",
-                "HCCL_BUFFSIZE": "2048",
-                "MOE_ENABLE_TOPK_NEG_ONE": "1",
-                **os.environ,
-            },
-        )
+OTHER_ARGS = [
+    "--trust-remote-code",
+    "--attention-backend",
+    "ascend",
+    "--quantization",
+    "modelslim",
+    "--tp-size",
+    "8",
+    "--moe-a2a-backend",
+    "deepep",
+    "--deepep-mode",
+    "low_latency",
+    "--max-running-requests",
+    128,
+    "--disable-cuda-graph",
+    "--dp-size",
+    8,
+    "--enable-dp-attention",
+    "--chunked-prefill-size",
+    1024,
+    "--mem-fraction-static",
+    0.68,
+]
 
-    @classmethod
-    def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
 
-    def test_mmlu(self):
-        expect_score = 0.58
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="mmlu",
-            num_examples=128,
-            num_threads=32,
-        )
-        print("Starting mmlu test...")
-        metrics = run_eval(args)
-        self.assertGreater(metrics["score"], expect_score)
+class TestDeepEpDeepseek(TestAscendAccuracyTestCaseBase):
+    """Testcase: Verify the accuracy of DeepSeek-V2 model on MMLU and GSM8K tasks with --deepep-mode low_latency on Ascend backend.
 
-    def test_gsm8k(self):
-        expect_accuracy = 0.34
-        args = SimpleNamespace(
-            num_shots=8,
-            data_path=None,
-            num_questions=200,
-            max_new_tokens=512,
-            parallel=128,
-            host="http://127.0.0.1",
-            port=int(self.base_url.split(":")[-1]),
-        )
-        print("Starting gsm8k test...")
-        metrics = run_gsm8k(args)
-        self.assertGreaterEqual(
-            metrics["accuracy"],
-            expect_accuracy,
-            f'Accuracy of {self.model} is {str(metrics["accuracy"])}, is lower than {expect_accuracy}',
-        )
+    [Test Category] Parameter
+    [Test Target] --moe-a2a-backend; --deepep-mode
+    """
+    model = DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
+    envs = ENVS
+    other_args = OTHER_ARGS
+
+    def test_gsm8k_accuracy(self):
+        self.dataset_type = "gsm8k"
+        self.dataset_name = "demo_gsm8k_gen_4_shot_cot_chat_prompt"
+        self.accuracy = 0.34
+        self.num_prompts = 200
+        self.run_accuracy()
+
+    def test_mmlu_accuracy(self):
+        self.dataset_type = "mmlu"
+        self.dataset_name = "mmlu_gen_5_shot_str"
+        self.accuracy = 0.58
+        self.num_prompts = 200
+        self.run_accuracy()
 
 
 if __name__ == "__main__":
