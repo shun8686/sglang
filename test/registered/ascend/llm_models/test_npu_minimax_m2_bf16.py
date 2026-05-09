3a443b7de483c2bd5e0a6b03288544966dc845bc
cy <chenyang08056032@163.com>
2026-05-09 17:43:32 +0800

    add llm model testcases

--- test/registered/ascend/llm_models/test_npu_minimax_m2_bf16.py
+++ test/registered/ascend/llm_models/test_npu_minimax_m2_bf16.py
@@ -1,33 +1,36 @@
 import unittest
 
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
-from sglang.test.ascend.test_ascend_utils import MINICPM3_4B_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import MINIMAX_M2_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
-register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+register_npu_ci(est_time=400, suite="full-8-npu-a3", nightly=True)
 
 
-class TestMiniCPM3(GSM8KAscendMixin, CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the OpenBMB/MiniCPM3-4B model on the GSM8K dataset is no less than 0.69.
+class TestMiniMaxM2(GSM8KAscendMixin, CustomTestCase):
+    """Testcase: Verify that the inference accuracy of the cyankiwi/MiniMax-M2-BF16 model on the GSM8K dataset is no less than 0.9.
 
     [Test Category] Model
-    [Test Target] OpenBMB/MiniCPM3-4B
+    [Test Target] cyankiwi/MiniMax-M2-BF16
     """
 
-    model = MINICPM3_4B_WEIGHTS_PATH
-    accuracy = 0.69
+    model = MINIMAX_M2_WEIGHTS_PATH
+    accuracy = 0.9
+    timeout_for_server_launch = 3000
     other_args = [
         "--trust-remote-code",
         "--mem-fraction-static",
-        "0.8",
+        "0.9",
         "--attention-backend",
         "ascend",
+        "--tp-size",
+        "8",
         "--disable-cuda-graph",
         "--disable-radix-cache",
         "--disable-overlap-schedule",
         "--max-running-requests",
-        "128",
+        "64",
         "--chunked-prefill-size",
         "-1",
     ]
