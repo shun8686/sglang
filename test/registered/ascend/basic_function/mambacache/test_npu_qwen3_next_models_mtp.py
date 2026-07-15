d7279715831aab8031f2a9340cdee7ca7c22bb61
l30056750 <1964551841@qq.com>
2026-07-15 14:53:00 +0800

    GPU case transplantation

---
+++ test/registered/ascend/basic_function/mambacache/test_npu_qwen3_next_models_mtp.py
@@ -0,0 +1,81 @@
+import unittest
+
+from sglang.test.ascend.test_ascend_utils import (
+    QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_FOR_TEST,
+)
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.kits.eval_accuracy_kit import GSM8KMixin
+from sglang.test.kits.kl_divergence_kit import KLDivergenceMixin
+from sglang.test.kits.prefix_cache_branching_kit import PrefixCacheBranchingMixin
+from sglang.test.server_fixtures.default_fixture import DefaultServerBase
+
+register_npu_ci(est_time=600, suite="full-4-npu-a3", nightly=True)
+
+QWEN3_NEXT_MODEL = QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_FOR_TEST.model_path
+
+
+class TestQwen3NextMTPTopk(
+    GSM8KMixin, KLDivergenceMixin, PrefixCacheBranchingMixin, DefaultServerBase
+):
+    model = QWEN3_NEXT_MODEL
+    cache_chunk_size = 64
+    gsm8k_accuracy_thres = 0.93
+    kl_div_thres = 0.008
+    other_args = [
+        "--trust-remote-code",
+        "--disable-cuda-graph",
+        "--speculative-algorithm",
+        "NEXTN",
+        "--speculative-num-steps",
+        "5",
+        "--speculative-eagle-topk",
+        "1",
+        "--speculative-num-draft-tokens",
+        "6",
+        "--mem-fraction-static",
+        "0.8",
+        "--tp",
+        "8",
+        "--chunked-prefill-size",
+        "2048",
+        "--mamba-scheduler-strategy",
+        "extra_buffer",
+        "--mamba-track-interval",
+        "128",
+        "--attention-backend",
+        "ascend",
+    ]
+
+
+class TestQwen3NextMTPV2(GSM8KMixin, KLDivergenceMixin, DefaultServerBase):
+    model = QWEN3_NEXT_MODEL
+    gsm8k_accuracy_thres = 0.93
+    kl_div_thres = 0.0035
+    other_args = [
+        "--trust-remote-code",
+        "--disable-cuda-graph",
+        "--speculative-algorithm",
+        "NEXTN",
+        "--speculative-num-steps",
+        "3",
+        "--speculative-eagle-topk",
+        "1",
+        "--speculative-num-draft-tokens",
+        "4",
+        "--mem-fraction-static",
+        "0.8",
+        "--tp",
+        "4",
+        "--chunked-prefill-size",
+        "2048",
+        "--mamba-scheduler-strategy",
+        "extra_buffer",
+        "--mamba-track-interval",
+        "128",
+        "--attention-backend",
+        "ascend",
+    ]
+
+
+if __name__ == "__main__":
+    unittest.main()
